e682604757d90deda4b8482474218250ad3796c7
asolar <none@none>
2004-10-10 14:29:56 -0700

    Latest working version

--- src/main/java/sketch/compiler/passes/lowering/GetExprType.java
+++ src/main/java/sketch/compiler/passes/lowering/GetExprType.java
@@ -17,6 +17,7 @@
 package streamit.frontend.nodes;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Visitor that returns the type of an expression.  This needs to be
@@ -30,11 +31,14 @@ public class GetExprType extends FENullVisitor
 {
     private SymbolTable symTab;
     private StreamType streamType;
+    private Map structsByName;
     
-    public GetExprType(SymbolTable symTab, StreamType streamType)
+    public GetExprType(SymbolTable symTab, StreamType streamType,
+                       Map structsByName)
     {
         this.symTab = symTab;
         this.streamType = streamType;
+        this.structsByName = structsByName;
     }
     
     public Object visitExprArray(ExprArray exp)
@@ -42,6 +46,32 @@ public class GetExprType extends FENullVisitor
         Type base = (Type)exp.getBase().accept(this);
         // ASSERT: base is a TypeArray.
         return ((TypeArray)base).getBase();
+    }
+
+    public Object visitExprArrayInit(ExprArrayInit exp)
+    {
+	// want to determine these about the array
+	Type base;
+	int length;
+
+	// get the elements
+	List elems = exp.getElements();
+
+	// not sure what to do for base type if array is empty... try
+	// keeping it null --BFT
+	if (elems.size()==0) {
+	    base = null;
+	} else {
+	    // otherwise, take promotion over all elements declared
+	    base = (Type)((Expression)elems.get(0)).accept(this);
+	    
+	    for (int i=1; i<elems.size(); i++) {
+		Type t = (Type)((Expression)elems.get(i)).accept(this);
+		base = t.leastCommonPromotion(t);
+	    }
+	}
+	
+	return new TypeArray(base, new ExprConstInt(elems.size()));
     }
 
     public Object visitExprBinary(ExprBinary exp)
@@ -58,9 +88,11 @@ public class GetExprType extends FENullVisitor
             return new TypePrimitive(TypePrimitive.TYPE_BOOLEAN);
         }
         
-        // Otherwise, this requires type unification.  Punt for the
-        // moment (though it wouldn't actually be hard).
-        return exp.getLeft().accept(this);
+        // The type of the expression is some type that both sides
+        // promote to, otherwise.
+        Type tl = (Type)exp.getLeft().accept(this);
+        Type tr = (Type)exp.getRight().accept(this);
+        return tl.leastCommonPromotion(tr);
     }
 
     public Object visitExprComplex(ExprComplex exp)
@@ -86,7 +118,12 @@ public class GetExprType extends FENullVisitor
 
     public Object visitExprConstInt(ExprConstInt exp)
     {
-        return new TypePrimitive(TypePrimitive.TYPE_INT);
+	// return a bit type if the value is 0 or 1
+	if (exp.getVal()==0 || exp.getVal()==1) {
+	    return new TypePrimitive(TypePrimitive.TYPE_BIT);
+	} else {
+	    return new TypePrimitive(TypePrimitive.TYPE_INT);
+	}
     }
     
     public Object visitExprConstStr(ExprConstStr exp)
@@ -98,11 +135,23 @@ public class GetExprType extends FENullVisitor
     public Object visitExprField(ExprField exp)
     {
         Type base = (Type)exp.getLeft().accept(this);
-        // If the base is a complex type, a field of it is double.
+        // If the base is a complex type, a field of it is float.
         if (base.isComplex())
-            return new TypePrimitive(TypePrimitive.TYPE_DOUBLE);
-        // ASSERT: base is a TypeStruct.
-        return ((TypeStruct)base).getType(exp.getName());
+            return new TypePrimitive(TypePrimitive.TYPE_FLOAT);
+        else if (base instanceof TypeStruct)
+            return ((TypeStruct)base).getType(exp.getName());
+        else if (base instanceof TypeStructRef)
+        {
+            String name = ((TypeStructRef)base).getName();
+            TypeStruct str = (TypeStruct)structsByName.get(name);
+            assert str != null : base;
+            return str.getType(exp.getName());
+        }
+        else
+        {
+            assert false : base;
+            return null;
+        }
     }
 
     public Object visitExprFunCall(ExprFunCall exp)
@@ -115,6 +164,14 @@ public class GetExprType extends FENullVisitor
         } catch (UnrecognizedVariableException e) {
             // ignore
         }
+
+	// "abs" returns a float.  We should probably insert other
+	// special cases here for built-in functions, but I'm not
+	// exactly sure which ones have a constant return type and
+	// which ones are polymorphic.  --BFT
+	if (exp.getName().equals("abs")) {
+	    return new TypePrimitive(TypePrimitive.TYPE_FLOAT);
+	}
         
         // Otherwise, we can assume that the only function calls are
         // calls to built-in functions in the absence of helper
@@ -126,8 +183,9 @@ public class GetExprType extends FENullVisitor
         // So, if there's any arguments, return the type of the first
         // argument; otherwise, return float as a default.
         List params = exp.getParams();
-        if (params.isEmpty())
+        if (params.isEmpty()) {
             return new TypePrimitive(TypePrimitive.TYPE_FLOAT);
+	}
         return ((Expression)params.get(0)).accept(this);
     }
     
@@ -143,9 +201,11 @@ public class GetExprType extends FENullVisitor
     
     public Object visitExprTernary(ExprTernary exp)
     {
-        // Again, should do type unification on the two sides.
-        // And might not want to blindly assert ?:.
-        return exp.getB().accept(this);
+        // Do type unification on the two sides.
+        // (Might not want to blindly assert ?:.)
+        Type tb = (Type)exp.getB().accept(this);
+        Type tc = (Type)exp.getC().accept(this);
+        return tb.leastCommonPromotion(tc);
     }
 
     public Object visitExprTypeCast(ExprTypeCast exp)
@@ -156,9 +216,28 @@ public class GetExprType extends FENullVisitor
     public Object visitExprUnary(ExprUnary exp)
     {
         // A little more solid ground here: the type of -foo and !foo
-        // will probably always be the same as the type of foo.
-        return exp.getExpr().accept(this);
+        // will be the same as type of foo, except for bits...
+	Type t = (Type)exp.getExpr().accept(this);
+	// if <t> is a bit, then the unary expression could promote it
+	// to an int.
+	if (t.equals(TypePrimitive.bittype)) {
+	    switch (exp.getOp()) {
+	    case ExprUnary.UNOP_NOT: {
+		// it is still a bit if it is negated, I think.
+		return TypePrimitive.bittype;
+	    }
+	    case ExprUnary.UNOP_NEG:
+	    case ExprUnary.UNOP_PREINC:
+	    case ExprUnary.UNOP_POSTINC:
+	    case ExprUnary.UNOP_PREDEC: 
+	    case ExprUnary.UNOP_POSTDEC: {
+		return TypePrimitive.inttype;
+	    }
+	    }
+	}
+	return t;
     }
+
     
     public Object visitExprVar(ExprVar exp)
     {
