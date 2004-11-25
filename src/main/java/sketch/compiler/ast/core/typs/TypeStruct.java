801593bdd6de7b65d6b7a82c7abae5843aae0a73
asolar <none@none>
2004-11-24 17:54:48 -0800

    *** empty log message ***

--- src/main/java/sketch/compiler/ast/core/typs/TypeStruct.java
+++ src/main/java/sketch/compiler/ast/core/typs/TypeStruct.java
@@ -16,10 +16,9 @@
 
 package streamit.frontend.nodes;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import java.util.HashMap;
 
 /**
  * A hetereogeneous structure type.  This type has a name for itself,
