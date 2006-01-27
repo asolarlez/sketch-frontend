2238d7c983d2887d35c12b3ff6ac2c25bd2e7120
asolar <none@none>
2006-01-27 12:37:17 -0800

    New Code Generation now working

--- src/main/java/sketch/compiler/solvers/constructs/ValueOracle.java
+++ src/main/java/sketch/compiler/solvers/constructs/ValueOracle.java
@@ -8,6 +8,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.Map.Entry;
 
 import streamit.frontend.nodes.ExprConstInt;
 import streamit.frontend.nodes.ExprStar;
@@ -15,8 +16,17 @@ import streamit.frontend.nodes.FENode;
 
 public class ValueOracle {
 	private Map<FENode, List<String>> store;	
+	private Map<FENode, Iterator<String>> currentVal;
 	private Map<String, Boolean> valMap;
 	private int starSizesCaped = -1;
+	
+	public void initCurrentVals(){
+		currentVal = new HashMap<FENode, Iterator<String>>();
+		for(Iterator<Entry<FENode, List<String>>> it = store.entrySet().iterator(); it.hasNext(); ){
+			Entry<FENode, List<String>> ent = it.next();
+			currentVal.put(ent.getKey(),ent.getValue().iterator());
+		}
+	}
 	
 	public void capStarSizes(int size){
 		starSizesCaped = size;
@@ -36,6 +46,43 @@ public class ValueOracle {
 			lst.add(vname);
 			store.put(node, lst);
 		}
+	}	
+	
+	public ExprConstInt popValueForNode(FENode node){
+		Iterator<String> cit = currentVal.get(node);
+		assert cit.hasNext() : "This can't happen";		
+		return getVal(node, cit.next());
+	}
+	
+	public ExprConstInt getVal(FENode node, String var){
+		if(node instanceof ExprStar){
+			ExprStar star = (ExprStar) node;
+			if(star.getSize() == 1){
+				Boolean val = valMap.get(var);
+				if(val != null){
+					return(new ExprConstInt(node.getContext(), val.booleanValue()?1:0));
+				}
+			}else{
+				int v = 0;
+				int p = 1;
+				int size=star.getSize();
+				if(starSizesCaped>0 && !star.isFixed()){
+					size = starSizesCaped;
+				}
+				for(int i=0; i<size; ++i){
+					boolean val = valMap.get(var + "_" + i).booleanValue();
+					v += p*(val?1:0);
+					p = p*2;
+				}
+				return(new ExprConstInt(node.getContext(), v));
+			}
+		}else{
+			Boolean val = valMap.get(var);
+			if(val != null){
+				return(new ExprConstInt(node.getContext(), val.booleanValue()?1:0));
+			}
+		}
+		return new ExprConstInt(node.getContext(), -1);
 	}
 	
 	public List<ExprConstInt> getVarsForNode(FENode node){
@@ -45,34 +92,7 @@ public class ValueOracle {
 		Iterator<String> it = varsForNode.iterator();
 		while(it.hasNext()){
 			String var = it.next();
-			if(node instanceof ExprStar){
-				ExprStar star = (ExprStar) node;
-				if(star.getSize() == 1){
-					Boolean val = valMap.get(var);
-					if(val != null){
-						result.add(new ExprConstInt(node.getContext(), val.booleanValue()?1:0));
-					}
-				}else{
-					int v = 0;
-					int p = 1;
-					int size=star.getSize();
-					if(starSizesCaped>0 && !star.isFixed()){
-						size = starSizesCaped;
-					}
-					for(int i=0; i<size; ++i){
-						boolean val = valMap.get(var + "_" + i).booleanValue();
-						v += p*(val?1:0);
-						p = p*2;
-					}
-					result.add(new ExprConstInt(node.getContext(), v));
-				}
-				
-			}else{
-				Boolean val = valMap.get(var);
-				if(val != null){
-					result.add(new ExprConstInt(node.getContext(), val.booleanValue()?1:0));
-				}
-			}
+			result.add(getVal(node, var));
 		}
 		return result;
 	}
