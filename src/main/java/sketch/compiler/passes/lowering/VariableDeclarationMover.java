ea6b11914d042554175189cd088b79f2e242cdc9
tancau <none@none>
2006-11-09 14:51:55 -0800

    var declaration mover for fortran codegen

---
+++ src/main/java/sketch/compiler/passes/lowering/VariableDeclarationMover.java
@@ -0,0 +1,70 @@
+package streamit.frontend.passes;
+
+import java.util.*;
+
+import streamit.frontend.nodes.*;
+
+/**
+ * Collects all variable declarations from a function (except for loop vars)
+ * and moves them to the top of that function. 
+ * It's a good idea to run VariableDisambiguator beforehand to avoid duplicate 
+ * variable names.
+ */
+public class VariableDeclarationMover extends FEReplacer
+{
+	private List<String> variables;
+    private Map<String,Type> varTypes;
+    
+    public VariableDeclarationMover()
+    {
+    	variables=new ArrayList<String>();
+    	varTypes=new HashMap<String,Type>();
+    }
+
+	public Object visitFunction(Function func)
+	{
+		variables.clear();
+		varTypes.clear();
+		func=(Function) super.visitFunction(func);
+		List bodyStmts=new ArrayList();
+		for(String name: variables) {
+			bodyStmts.add(new StmtVarDecl(null, varTypes.get(name), name, null));
+		}
+		StmtBlock bodyBlock=(StmtBlock) func.getBody();
+		bodyStmts.addAll(bodyBlock.getStmts());
+		variables.clear();
+		varTypes.clear();
+		return new Function(func.getContext(), func.getCls(), func.getName(),
+			func.getReturnType(), func.getParams(), func.getSpecification(), 
+			new StmtBlock(bodyBlock.getContext(), bodyStmts));
+	}
+
+	public Object visitStmtFor(StmtFor stmt)
+	{
+		// we skip visiting the loop variable declaration
+        Statement newBody = (Statement)stmt.getBody().accept(this);
+        if(newBody == stmt.getBody())
+            return stmt;
+        return new StmtFor(stmt.getContext(), stmt.getInit(), stmt.getCond(), 
+        	stmt.getIncr(), newBody);
+	}
+
+	public Object visitStmtVarDecl(StmtVarDecl stmt)
+	{
+		for(int i=0;i<stmt.getNumVars();i++) {
+			String name=stmt.getName(i);
+			if(varTypes.get(name)==null) {
+				variables.add(name);
+				varTypes.put(name,stmt.getType(i));
+				if(stmt.getInit(i)!=null) {
+					addStatement(new StmtAssign(stmt.getContext(),
+						new ExprVar(null,name), stmt.getInit(i)));
+				}
+			} else {
+				assert false: "duplicate variable declaration for"+name+": "+stmt+" at "+stmt.getContext();
+			}
+		}
+		return null;
+	}
+
+}
