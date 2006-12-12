d3f0b5d249594e855e023d7976987d76d39fdd04
asolar <none@none>
2006-12-12 15:01:14 -0800

    *** empty log message ***

---
+++ src/main/java/sketch/compiler/stencilSK/MatchParamNames.java
@@ -0,0 +1,43 @@
+package streamit.frontend.stencilSK;
+
+import java.util.List;
+
+import streamit.frontend.nodes.FEReplacer;
+import streamit.frontend.nodes.Function;
+import streamit.frontend.nodes.Parameter;
+import streamit.frontend.nodes.Statement;
+/**
+ * This class makes sure that the names in the parameter list for both the spec
+ * and the sketch match.
+ * @author asolar
+ *
+ */
+public class MatchParamNames extends FEReplacer {
+
+	
+	
+    public Object visitFunction(Function func)
+    {
+    	Statement newBody = func.getBody();
+    	List<Parameter>  newParams = func.getParams();
+    	if( func.getSpecification() != null ){
+    		Function spec = this.getFuncNamed(func.getSpecification());
+    		assert func.getParams().size() == spec.getParams().size() : spec.toString() + ": The size of the parameter lists for spec and sketch must match. "; 
+    		List<Parameter> p1 = func.getParams();
+    		List<Parameter> p2 = spec.getParams();    		
+    		for(int i=0; i< p2.size() ; ++i ){
+    			//TODO There should be a type check here.
+    			String oldName = p1.get(i).getName();
+    			String newName = p2.get(i).getName();
+    			if( !oldName.equals(newName) ){
+    				newBody = (Statement) newBody.accept( new VarReplacer(oldName, newName ) );
+    				newParams = p2;
+    			}
+    		}    		
+    	}
+        if (newBody == func.getBody()) return func;
+        return new Function(func.getContext(), func.getCls(),
+                            func.getName(), func.getReturnType(),
+                            newParams, func.getSpecification(), newBody);
+    }
+}
--- src/main/java/sketch/compiler/stencilSK/VarReplacer.java
+++ src/main/java/sketch/compiler/stencilSK/VarReplacer.java
@@ -1,14 +1,26 @@
 package streamit.frontend.stencilSK;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import streamit.frontend.nodes.ExprVar;
 import streamit.frontend.nodes.Expression;
 import streamit.frontend.nodes.FEReplacer;
+import streamit.frontend.nodes.Statement;
+import streamit.frontend.nodes.StmtBlock;
+import streamit.frontend.nodes.StmtVarDecl;
 
 
 
 class VarReplacer extends FEReplacer{
 	String oldName;
 	Expression newName;
+	
+	VarReplacer(String oldName, String newName){
+		this.oldName = oldName;
+		this.newName = new ExprVar(null, newName);
+	}
 	
 	VarReplacer(String oldName, Expression newName){
 		this.oldName = oldName;
@@ -22,6 +34,73 @@ class VarReplacer extends FEReplacer{
 			return exp;
 		}
 	}
+	
+	
+	public Object visitStmtBlock(StmtBlock stmt)
+    {
+        List<Statement> oldStatements = newStatements;
+        newStatements = new ArrayList<Statement>();
+        for (Iterator iter = stmt.getStmts().iterator(); iter.hasNext(); )
+        {
+            Statement s = (Statement)iter.next();
+            // completely ignore null statements, causing them to
+            // be dropped in the output
+            if (s == null)
+                continue;
+            doStatement(s);
+        }
+        Statement result = new StmtBlock(stmt.getContext(), newStatements);
+        newStatements = oldStatements;
+        return result;
+    }
+	
+	
+	public Object visitStmtVarDecl(StmtVarDecl stmt)
+    {
+        List<Expression> newInits = new ArrayList<Expression>();
+        List<String> newNames = new ArrayList<String>();
+        boolean changed = false;
+        for (int i = 0; i < stmt.getNumVars(); i++)
+        {
+            Expression init = stmt.getInit(i);
+            Expression oinit = init;
+            if (init != null)
+                init = doExpression(init);
+            if( oinit != init) changed = true;
+            newInits.add(init);
+            String name = stmt.getName(i);
+            
+            if( newName instanceof ExprVar ){
+            	String sNewName = ((ExprVar)newName).getName();
+            	if( name.equals(oldName) ){
+            		name = sNewName;
+            		changed = true;
+            	}
+            	
+            	if( name.equals(sNewName) ){
+            		//In this case, we need to rename this variable with a fresh name.
+            		//This implies going back to the enclosing scope, and renaming all
+            		//uses of this variable. Not that easy.
+            		assert false : "This has not yet been implemented. Please implemented.";
+            	}            	
+            }else{
+            	//In this case, we have to search through all the variables that appear
+            	//in newName, and if any of them match name, we must rename this variable
+            	//with a fresh name.
+            	assert false : "Not yet implemented";
+            	if( name.equals(oldName) ){
+            		assert false : "This has not been implemented";
+            	}
+            }
+            
+            
+            
+            newNames.add(name);
+        }
+        if( !changed ) return stmt;
+        return new StmtVarDecl(stmt.getContext(), stmt.getTypes(),
+                               stmt.getNames(), newInits);
+    }
 	
 }
 
