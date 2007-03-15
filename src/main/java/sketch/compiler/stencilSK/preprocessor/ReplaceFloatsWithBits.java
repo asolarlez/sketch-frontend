a7cf4efd5f8e03109d7d697d4c0565ea74f18d0e
asolar <none@none>
2007-03-14 22:28:58 -0700

    Removes floats and replaces them with bits.

---
+++ src/main/java/sketch/compiler/stencilSK/preprocessor/ReplaceFloatsWithBits.java
@@ -0,0 +1,105 @@
+package streamit.frontend.stencilSK.preprocessor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import streamit.frontend.nodes.ExprBinary;
+import streamit.frontend.nodes.ExprConstFloat;
+import streamit.frontend.nodes.ExprFunCall;
+import streamit.frontend.nodes.ExprVar;
+import streamit.frontend.nodes.Expression;
+import streamit.frontend.nodes.Function;
+import streamit.frontend.nodes.Parameter;
+import streamit.frontend.nodes.StreamSpec;
+import streamit.frontend.nodes.Type;
+import streamit.frontend.nodes.TypePrimitive;
+import streamit.frontend.passes.SymbolTableVisitor;
+
+public class ReplaceFloatsWithBits extends SymbolTableVisitor{
+	
+	public static final Type FLOAT = TypePrimitive.floattype;
+	
+	
+	Map<Float, Function> floatConstants = new HashMap<Float, Function>();		
+	
+	public ReplaceFloatsWithBits(){
+		super(null);
+	}
+	
+	
+	Function newFloatFunction(String flName){
+		return Function.newUninterp(flName, TypePrimitive.bittype, new ArrayList<Parameter>(0));
+	}
+	
+	
+	String fName(Float fl){
+		String name = fl.toString();
+		name  = name.replace('.', '_');
+		return "FL_" + name;
+	}
+	
+	public Object visitExprConstFloat(ExprConstFloat fexp){
+		String name = null;
+		
+		Float fl = new Float(fexp.getVal());
+		if(floatConstants.containsKey(fl)){
+			name = floatConstants.get(fl).getName();
+		}else{
+			name = fName(fl);			
+			floatConstants.put(fl, newFloatFunction(name));			
+		}
+		return new ExprFunCall(fexp.getCx(), name, new ArrayList<Parameter>(0) );
+	}
+	
+	
+	
+	
+	
+	public Object visitTypePrimitive(TypePrimitive t) {
+		if(t.equals(FLOAT)){
+			return TypePrimitive.bittype;
+		}
+    	return t; 
+    }
+	
+	
+	public Object visitExprBinary(ExprBinary exp)
+    {
+		Type ltype = getType(exp.getLeft());
+		Type rtype = getType(exp.getRight());
+		
+		if(!ltype.equals(FLOAT) && !rtype.equals(FLOAT)){
+			return super.visitExprBinary(exp);
+		}		 
+        Expression left = doExpression(exp.getLeft());
+        Expression right = doExpression(exp.getRight());
+        int newOp = exp.getOp();
+        switch(exp.getOp()){
+        	case ExprBinary.BINOP_ADD: newOp = ExprBinary.BINOP_BXOR; break;
+        	case ExprBinary.BINOP_SUB: newOp = ExprBinary.BINOP_BXOR; break;
+        	case ExprBinary.BINOP_MUL: newOp = ExprBinary.BINOP_BAND; break;
+        	default:
+        		assert false : "You can't apply this floating point operation if you are doing floating-point to boolean replacement." + exp;
+        }        
+        if (left == exp.getLeft() && right == exp.getRight() && newOp == exp.getOp())
+            return exp;
+        else
+            return new ExprBinary(exp.getContext(), newOp, left, right, exp.getAlias());
+    }
+	
+	public Object visitStreamSpec(StreamSpec spec)
+    {
+		spec = (StreamSpec)super.visitStreamSpec(spec);		
+		spec.getFuncs().addAll(floatConstants.values());
+		return spec;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+}
