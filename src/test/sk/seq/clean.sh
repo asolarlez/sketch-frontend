501302a937756d1ffde613e73274a30f1858407c
arnold <none@none>
2007-07-18 14:33:51 -0700

    added test cases for bug #41

--- src/test/sk/seq/clean.sh
+++ src/test/sk/seq/clean.sh
@@ -1,5 +1,5 @@
-#! /usr/bin/bash 
-rm -f script *.h *.exe *.stackdump *.c *.cpp *.cc *.sout *.output 
+#!/bin/bash 
+rm -f script *.h *.exe *.stackdump *.c *.cpp *.cc *.sout *.output *.tmp
 for FILE in *.sk; do rm -f ${FILE%.sk}; done
 
 
