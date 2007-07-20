5ae57bd16efe0ac121bc7a6302e8ca5c90fb8b85
asolar <none@none>
2007-07-19 23:42:39 -0700

    Changes to fix problem with array initialization.

--- src/main/java/sketch/compiler/stencilSK/EliminateStarStatic.java
+++ src/main/java/sketch/compiler/stencilSK/EliminateStarStatic.java
@@ -1,7 +1,15 @@
 package streamit.frontend.stencilSK;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import streamit.frontend.experimental.abstractValue;
+import streamit.frontend.nodes.ExprArrayInit;
+import streamit.frontend.nodes.ExprConstInt;
 import streamit.frontend.nodes.ExprStar;
 import streamit.frontend.nodes.FEReplacer;
+import streamit.frontend.nodes.Type;
+import streamit.frontend.nodes.TypeArray;
 import streamit.frontend.tosbit.ValueOracle;
 
 public class EliminateStarStatic extends FEReplacer {
@@ -15,7 +23,22 @@ public class EliminateStarStatic extends FEReplacer {
 	}
 	
 	public Object visitExprStar(ExprStar star) {
-		return oracle.popValueForNode(star);
+		Type t = star.getType();
+		int ssz = 1;
+		if(t instanceof TypeArray){
+			Integer iv = ((TypeArray)t).getLength().getIValue();
+			assert iv != null;
+			ssz = iv;
+			List<ExprConstInt> lst = new ArrayList<ExprConstInt>(ssz);
+			for(int i=0; i<ssz; ++i){
+				lst.add(oracle.popValueForNode(star.getDepObject(i)));				
+			}
+			
+			ExprArrayInit ainit = new ExprArrayInit(star.getCx(), lst);
+			return ainit;			
+		}else{
+			return oracle.popValueForNode(star.getDepObject(0));
+		}
 	}
 	
 }
