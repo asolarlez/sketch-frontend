c809251c13961b18ef9c734d7d37c7d9e4ca41b4
asolar <none@none>
2007-07-19 23:42:14 -0700

    Changes to fix problem with array initialization.

---
+++ src/main/java/sketch/compiler/ast/core/DummyFENode.java
@@ -0,0 +1,19 @@
+package streamit.frontend.nodes;
+
+public class DummyFENode extends FENode {
+
+	public DummyFENode(FENode node) {
+		super(node);
+	}
+
+	public DummyFENode(FEContext context) {
+		super(context);
+	}
+
+	@Override
+	public Object accept(FEVisitor v) {
+		assert false : "This is a dummy class; you shouldn't be calling it's visitor.";
+		return null;
+	}
+
+}
