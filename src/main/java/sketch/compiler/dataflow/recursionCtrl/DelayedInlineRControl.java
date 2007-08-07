251a8cbf09b250e2fb42babee41aa28cf28bba89
asolar <none@none>
2007-08-07 01:13:57 -0700

    *** empty log message ***

---
+++ src/main/java/sketch/compiler/dataflow/recursionCtrl/DelayedInlineRControl.java
@@ -0,0 +1,36 @@
+package streamit.frontend.tosbit.recursionCtrl;
+
+import java.util.HashMap;
+
+import streamit.frontend.nodes.ExprFunCall;
+import streamit.frontend.nodes.Function;
+import streamit.frontend.nodes.Statement;
+import streamit.frontend.tosbit.recursionCtrl.BaseRControl.InlineCounter;
+
+public class DelayedInlineRControl extends BaseRControl {
+		
+		
+	public InlineCounter appearenceCounter = new InlineCounter();	
+	private int MAX_OCC;
+	
+	public DelayedInlineRControl(int maxInline, int maxOccurrence){
+		super(maxInline);		
+		MAX_OCC = maxOccurrence;
+	}
+	
+	
+
+
+	public void pushFunCall(ExprFunCall fc, Function fun) {
+		icount.incInlineCounter(fc.getName());
+		appearenceCounter.incInlineCounter(fc.getName());
+	}
+	
+
+	public boolean testCall(ExprFunCall fc) {
+		return super.testCall(fc) && appearenceCounter.getInlineCounter(fc.getName()) < MAX_OCC;
+	}
+	
+	
+
+}
