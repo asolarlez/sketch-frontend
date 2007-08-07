3dd2a31ed15b1a1e25c0f6d2062144d2e9f2e060
asolar <none@none>
2007-08-07 01:13:57 -0700

    *** empty log message ***

--- src/main/java/sketch/compiler/dataflow/recursionCtrl/BaseRControl.java
+++ src/main/java/sketch/compiler/dataflow/recursionCtrl/BaseRControl.java
@@ -7,8 +7,42 @@ import streamit.frontend.nodes.Function;
 import streamit.frontend.nodes.Statement;
 
 public class BaseRControl extends RecursionControl {
+	protected class InlineCounter{
+		private HashMap<String, Integer> inlineCounter = new HashMap<String, Integer> ();	
+		public int getInlineCounter (String funcName)
+	    {
+	        assert (funcName != null);
+	        Integer numInlinedInteger = inlineCounter.get (funcName);
+	        return (numInlinedInteger != null ? numInlinedInteger.intValue () : 0);
+	    }
 
-	private HashMap<String, Integer> inlineCounter = new HashMap<String, Integer> ();
+	    private int addInlineCounter (String funcName, int step) {
+	        int numInlined = getInlineCounter (funcName);
+	        numInlined += step;
+	        assert (numInlined >= 0) : " This is a problem  " + funcName + ", " + step + ", " + numInlined;
+	        inlineCounter.put (funcName, new Integer (numInlined));
+	        return numInlined;
+	    }
+
+	    private int incInlineCounter (String funcName, int step) {
+	        assert (step > 0);
+	        return addInlineCounter (funcName, step);
+	    }
+
+	    public int incInlineCounter (String funcName) {
+	        return incInlineCounter (funcName, 1);
+	    }
+
+	    private int decInlineCounter (String funcName, int step) {
+	        assert (step > 0);
+	        return addInlineCounter (funcName, -step);
+	    }
+
+	    public int decInlineCounter (String funcName) {
+	        return decInlineCounter (funcName, 1);
+	    }
+	}
+	public InlineCounter icount = new InlineCounter();	
 	private int MAX_INLINE;
 	
 	public BaseRControl(int maxInline){
@@ -17,17 +51,17 @@ public class BaseRControl extends RecursionControl {
 	
 	
 	public int inlineLevel(ExprFunCall fun) {
-		return getInlineCounter(fun.getName());
+		return icount.getInlineCounter(fun.getName());
 	}
 
 	
 	public void popFunCall(ExprFunCall fun) {
-		decInlineCounter(fun.getName());
+		icount.decInlineCounter(fun.getName());
 	}
 
 	
 	public void pushFunCall(ExprFunCall fc, Function fun) {
-		incInlineCounter(fc.getName());
+		icount.incInlineCounter(fc.getName());
 	}
 
 	
@@ -42,40 +76,4 @@ public class BaseRControl extends RecursionControl {
 	public boolean testCall(ExprFunCall fc) {
 		return inlineLevel(fc) < MAX_INLINE ;
 	}
-	
-	
-	
-	
-	private int getInlineCounter (String funcName)
-    {
-        assert (funcName != null);
-        Integer numInlinedInteger = inlineCounter.get (funcName);
-        return (numInlinedInteger != null ? numInlinedInteger.intValue () : 0);
-    }
-
-    private int addInlineCounter (String funcName, int step) {
-        int numInlined = getInlineCounter (funcName);
-        numInlined += step;
-        assert (numInlined >= 0) : " This is a problem  " + funcName + ", " + step + ", " + numInlined;
-        inlineCounter.put (funcName, new Integer (numInlined));
-        return numInlined;
-    }
-
-    private int incInlineCounter (String funcName, int step) {
-        assert (step > 0);
-        return addInlineCounter (funcName, step);
-    }
-
-    private int incInlineCounter (String funcName) {
-        return incInlineCounter (funcName, 1);
-    }
-
-    private int decInlineCounter (String funcName, int step) {
-        assert (step > 0);
-        return addInlineCounter (funcName, -step);
-    }
-
-    private int decInlineCounter (String funcName) {
-        return decInlineCounter (funcName, 1);
-    }
 }
