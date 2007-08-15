b665616a6b4785f4de5b948ddead46cbf42224cb
asolar <none@none>
2007-08-14 20:02:47 -0700

    Fixed major bug involving the ? operator.

--- src/main/java/sketch/compiler/passes/lowering/EliminateBitSelector.java
+++ src/main/java/sketch/compiler/passes/lowering/EliminateBitSelector.java
@@ -3,22 +3,50 @@ package streamit.frontend.passes;
 import streamit.frontend.nodes.ExprBinary;
 import streamit.frontend.nodes.ExprStar;
 import streamit.frontend.nodes.ExprTernary;
-import streamit.frontend.nodes.FEReplacer;
+import streamit.frontend.nodes.ExprUnary;
+import streamit.frontend.nodes.ExprVar;
+import streamit.frontend.nodes.FEContext;
+import streamit.frontend.nodes.StmtAssign;
+import streamit.frontend.nodes.StmtVarDecl;
+import streamit.frontend.nodes.TempVarGen;
+import streamit.frontend.nodes.Type;
+import streamit.frontend.nodes.TypeArray;
+import streamit.frontend.nodes.TypePrimitive;
 
 public class EliminateBitSelector extends SymbolTableVisitor {
 	
-	
-	public EliminateBitSelector(){
+	TempVarGen varGen;
+	public EliminateBitSelector(TempVarGen varGen){
 		super(null);
+		this.varGen = varGen;
 	}
 	
     public Object visitExprBinary(ExprBinary exp)
     {
       if(exp.getOp() == ExprBinary.BINOP_SELECT){
-    	  ExprStar star = new ExprStar(exp.getCx());    	  
-    	  star.setType( this.getType(exp.getRight()) );
-    	  return new ExprTernary(exp.getCx(), ExprTernary.TEROP_COND,
-    			  star, exp.getLeft(), exp.getRight()); 
+    	  String nm = varGen.nextVar();
+    	  ExprVar tmpvar = new ExprVar(exp.getCx(), nm);
+    	  Type bvtype = this.getType(exp.getRight());
+    	  Type tmptype = bvtype;
+    	  
+    	  while(tmptype instanceof TypeArray){
+    		  tmptype = ((TypeArray) tmptype).getBase();
+    	  }
+    	  assert tmptype.equals(TypePrimitive.bittype) : "The {|} operator is currently only defined for bit vectors: " + exp.getCx();
+    	  StmtVarDecl decl = new StmtVarDecl(exp.getCx(), bvtype , nm, null  );    	  
+    	  this.addStatement( decl );
+    	  ExprStar star = new ExprStar(exp.getCx());
+    	  star.setType( bvtype );
+    	  StmtAssign ass = new StmtAssign(exp.getCx(), tmpvar, star );
+    	  this.addStatement(ass);
+
+    	  FEContext c = exp.getCx();
+    	  
+    	  return new ExprBinary(c, ExprBinary.BINOP_BOR,
+    		new ExprBinary(c,  ExprBinary.BINOP_BAND,exp.getLeft(), tmpvar)
+    	  ,
+    	    new ExprBinary(c,  ExprBinary.BINOP_BAND,exp.getRight(), new ExprUnary(c, ExprUnary.UNOP_BNOT, tmpvar  ) )
+    	  );
       }else{
     	  return super.visitExprBinary(exp);
       }
