68a33577a822b0684e6fcd419baa78a5e306710c
asolar <none@none>
2007-08-23 01:07:53 -0700

    *** empty log message ***

--- src/test/sk/seq/regtest.sh
+++ src/test/sk/seq/regtest.sh
@@ -9,7 +9,7 @@ rm -f ${bname}.cpp
 rm -f ${bname}.h
 
 
-bash preproc.sh --outputfiles -synth ABC -verif ABC  --incremental 6 --seed 10 ${x} &> ${x}.output ;
+bash preproc.sh  -synth ABC -verif ABC  --incremental 6 --seed 10 ${x} &> ${x}.output ;
 grep oracle.*\[[0-9]+\] ${x}.output ;
 done;
 
@@ -17,5 +17,9 @@ rm *.tmp;
 
 grep -n '[0-9]' *.cpp | cpp | sed 's/:[0-9]*:/::/' > current.output;
 
-diff -w current.output reference;
-
+# diff -w current.output reference;
+grep 'CORRECT' *.output | tr ':' ' ' | awk '{ print $1; }' > cur
+echo "LISTED BELOW ARE THE FAILED TESTS (IF ANY)"
+diff cur ref 
+echo "END OF LIST"
+rm cur
