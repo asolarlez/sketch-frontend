05abfcda5d25352d8d4dcc4fe658de846a0f16c0
asolar <none@none>
2007-08-23 17:52:14 -0700

    Upgraded code generator to use Gilad's bitvec library.

--- src/main/java/sketch/compiler/codegenerators/NodesToC.java
+++ src/main/java/sketch/compiler/codegenerators/NodesToC.java
@@ -7,125 +7,13 @@ import streamit.frontend.tojava.NodesToJava;
 
 public class NodesToC extends NodesToJava {
 
-	private String filename; 
-	private ArrayList<String> usedMacros=new ArrayList<String>();
-	private static HashMap<String,String> macroDefinitions=null;
+	private String filename; 	
 	
 	public NodesToC(TempVarGen varGen, String filename) {
 		super(false, varGen);
-		this.filename=filename;
-		if(macroDefinitions==null) {
-			macroDefinitions=new HashMap<String,String>();
-			macroDefinitions.put("SK_BITASSIGN","#define SK_BITASSIGN(a,i,x) a=((a)&(~(((unsigned long long)1)<<(i))))|(((x)&1)<<(i))");
-			macroDefinitions.put("SK_ONES","#define SK_ONES(n) ((((unsigned long long)1)<<(n))-1)");
-			macroDefinitions.put("SK_ONES_SL","#define SK_ONES_SL(s,l) (SK_ONES(l)<<(s))");
-			macroDefinitions.put("SK_ONES_SE","#define SK_ONES_SE(s,e) (SK_ONES((e)+1)^SK_ONES(s))");
-			macroDefinitions.put("SK_ONES_S","#define SK_ONES_S(s) (~SK_ONES(s))");
-			macroDefinitions.put("SK_ONES_E","#define SK_ONES_E(e) (SK_ONES((e)+1))");
-			macroDefinitions.put("SK_COPYBITS","#define SK_COPYBITS(l,m,r) l=((l)&~(m))|((r)&(m))");
-			macroDefinitions.put("SK_bitArrayCopy","template<typename T, typename V> \n"+
-					"void SK_bitArrayCopy(T* lhs, int s, int e, V* x, int ws)\n"+
-					"{\n	int aw=s/ws;\n	if(aw==e/ws) {\n	"+
-					"	int mask=SK_ONES_SE(s%ws,e%ws);\n"+
-					"		SK_COPYBITS(lhs[aw],mask,x[0]<<(s%ws));\n	}\n"+
-					"	else {\n"+
-					"		int k=s%ws;\n"+
-					"		int l=e-s;\n"+
-					"		int nfw=(l)/ws;\n"+
-					"       l = l+1;\n"+
-					"		int xw;\n"+
-					"		if(k==0) {\n"+
-					"			for(xw=0;xw<nfw;xw++) {\n"+
-					"				lhs[aw++]=x[xw];\n"+
-					"			}\n"+
-					"			int mask=SK_ONES_E(e%ws);\n"+
-					"			SK_COPYBITS(lhs[aw],mask,x[xw]);\n"+
-					"		}\n		else {\n			int kc=ws-k;\n"+
-					"			int mask1=SK_ONES_S(k);\n"+
-					"			int mask2=~mask1;\n"+
-					"			for(xw=0;xw<nfw;xw++) {\n"+
-					"				SK_COPYBITS(lhs[aw],mask1,x[xw]<<k);\n"+
-					"				aw++;\n"+
-					"				SK_COPYBITS(lhs[aw],mask2,x[xw]>>kc);\n"+
-					"			}\n"+
-					"			if(l%ws>kc) {\n"+
-					"				SK_COPYBITS(lhs[aw],mask1,x[xw]<<k);\n"+
-					"				aw++;\n"+
-					"				int mask=SK_ONES_E(e%ws);\n"+
-					"				SK_COPYBITS(lhs[aw],mask,x[xw]>>kc);\n"+
-					"			}\n"+
-					"			else {\n"+
-					"				int mask=SK_ONES_SE(k,e%ws);\n"+
-					"				SK_COPYBITS(lhs[aw],mask,x[xw]<<k);\n"+
-					"			}\n"+
-					"		}\n	}\n}\n");
-			macroDefinitions.put("SK_bitArrayCopyInv","template<typename T, typename V> \n" +
-					"void SK_bitArrayCopyInv(T* lhs, int s, int e, V* x, int wsl, int wsr, int totlen){\n" +
-					"    int aw=s/wsr;\n" +
-					"    int l=e-s+1;\n" +
-					"    if(aw==e/wsr) {\n" +
-					"       if( (l-1)/wsl == 0 ){\n" +
-					"           if( l == wsr ){ lhs[0] = x[aw]; return; } \n"+
-					"           int mask=SK_ONES_SE(0,(e%wsr) - (s%wsr));\n" +
-					"           SK_COPYBITS(lhs[0],mask,x[aw]>>(s%wsr));\n" +
-					"       }else{\n" +
-					"          printf(\"THIS HAS NOT BEEN IMPLEMENTED\"); return;\n" +
-					"       }\n" +
-					"    }\n" +
-					"    else {\n" +
-					"       int ws = wsr;\n" +
-					"       if( wsr != wsl ){ printf(\"THIS HAS NOT BEEN IMPLEMENTED\"); return;}\n" +
-					"       int k=s%ws;\n" +
-					"       int nfw=(l-1)/ws;\n" +
-					"       int xw;\n" +
-					"       if(k==0) {\n" +
-					"           for(xw=0;xw<nfw;xw++) {\n" +
-					"               lhs[xw]=x[aw++];\n" +
-					"           }\n" +
-					"           int mask=SK_ONES_E(e%ws);\n" +
-					"           SK_COPYBITS(lhs[xw],mask,x[aw]);\n" +
-					"       }\n" +
-					"       else {\n" +
-					"           int kc=ws-k;\n" +
-					"           int mask1=SK_ONES(kc);\n" +
-					"           int mask2=~mask1;\n" +
-					"           for(xw=0;xw<nfw;xw++) {\n" +
-					"               SK_COPYBITS(lhs[xw],mask1,x[aw]>>k);\n" +
-					"               aw++;\n" +
-					"               SK_COPYBITS(lhs[xw],mask2,x[aw]<<kc);\n" +
-					"           }\n" +
-					"           if(l%ws>kc) {\n" +
-					"               SK_COPYBITS(lhs[aw],mask1,x[xw]>>k);\n" +
-					"               aw++;\n" +
-					"               int mask=SK_ONES_SE(kc, e%ws);\n" +
-					"               SK_COPYBITS(lhs[aw],mask,x[xw]<<kc);\n" +
-					"           }\n" +
-					"           else {\n" +
-					"               int mask=SK_ONES_E((e%ws)-k);\n" +
-					"               SK_COPYBITS(lhs[xw],mask,x[aw]>>k);\n" +
-					"           }\n" +
-					"       }\n" +
-					"    }\n" +
-					"    if(totlen > l){       for(int i=l; i<totlen; ++i) SK_BITASSIGN(lhs[i/wsl], i%wsl, 0);     }}\n" +
-					"\n" );
-		}
+		this.filename=filename;		
 	}
-	
-	private void requireMacro(String m) {
-		if(usedMacros.contains(m)) return;
-		if(m.startsWith("SK_ONES_")) requireMacro("SK_ONES");
-		else if(m.equals("SK_bitArrayCopy") || m.equals("SK_bitArrayCopyInv")) {
-			requireMacro("SK_COPYBITS");
-			requireMacro("SK_ONES_SE");
-			requireMacro("SK_ONES_S");
-			requireMacro("SK_ONES_E");
-			requireMacro("SK_BITASSIGN");
 			
-		}
-		assert macroDefinitions.containsKey(m);
-		usedMacros.add(m);
-	}
-	
 	@Override
 	public Object visitExprUnary(ExprUnary exp)
 	{
@@ -144,10 +32,6 @@ public class NodesToC extends NodesToJava {
 		preamble.append("#include \"");
 		preamble.append(filename);
 		preamble.append(".h\"\n");
-		for(Iterator<String> it=usedMacros.iterator();it.hasNext();) {
-			preamble.append(macroDefinitions.get(it.next()));
-			preamble.append("\n");
-		}
 		preamble.append(ret);
 		return preamble.toString();
 	}
@@ -177,6 +61,7 @@ public class NodesToC extends NodesToJava {
     }
 	
 
+	
     public String doParams(List params, String prefix)
     {
         String result = "(";
@@ -185,18 +70,11 @@ public class NodesToC extends NodesToJava {
         {
             Parameter param = (Parameter)iter.next();
             Type type = param.getType();
-            String postFix = "";
-            if(type instanceof TypeArray){
-            	postFix = "*";
-            	type = ((TypeArray)type).getBase();
-            }
+            
             
             if (!first) result += ", ";
             if (prefix != null) result += prefix + " ";
-            result += convertType(type) + postFix;
-            if(param.getType() instanceof TypePrimitive){
-            	result += "&";
-            }
+            result += typeForParam(type);            
             result += " ";
             result += param.getName();
             first = false;
@@ -205,23 +83,48 @@ public class NodesToC extends NodesToJava {
         return result;
     }
 
+    public String typeForParam(Type type){
+        if(type instanceof TypeArray){
+        	TypeArray otype = (TypeArray)type;
+        	type = ((TypeArray)type).getBase();
+        	if(type.equals(TypePrimitive.bittype)){
+        		return "bitvec<" + (otype).getLength() + "> ";
+        	}else{
+        		return convertType(type) + "*";
+        	}
+        }else{
+        	return convertType(type) + "&";
+        }
+    }
+    
+    
+    
+    public String typeForDecl(Type type, String name){
+    	String postFix = "";
+        if(type instanceof TypeArray){
+        	Type otype = type;
+        	type = ((TypeArray)type).getBase();
+        	if(type.equals(TypePrimitive.bittype)){
+        		return "bitvec<" + ((TypeArray)otype).getLength() + "> " + name + postFix ;
+        	}else{
+        		postFix = "[" + ((TypeArray)otype).getLength() +  "]";        	
+        		return convertType(type) +  " " + name + postFix ;
+        	}
+        }
+        return convertType(type) +  " " + name + postFix ;
+    }
+    
+    
     public Object visitStmtVarDecl(StmtVarDecl stmt)
     {
-        String result = "";
-        Type type = stmt.getType(0);
-        String postFix = "";
-        if(type instanceof TypeArray){
-        	postFix = "[" + ((TypeArray)type).getLength() +  "]";
-        	type = ((TypeArray)type).getBase();
-        }
-        result += convertType(type) +  " ";
+        String result = "";        
         for (int i = 0; i < stmt.getNumVars(); i++)
-        {
-            if (i > 0)
-                result += ", ";
-            result += stmt.getName(i)+ postFix ;
+        {            
+            Type type = stmt.getType(i);
+            result += typeForDecl(type,   stmt.getName(i));
             if (stmt.getInit(i) != null)
                 result += " = " + (String)stmt.getInit(i).accept(this);
+            result += ";\n";
         }
         return result;
     }
@@ -229,10 +132,7 @@ public class NodesToC extends NodesToJava {
 	public Object visitExprFunCall(ExprFunCall exp)
     {
 		String result = "";
-        String name = exp.getName();
-        if(macroDefinitions.containsKey(name)) {
-        	requireMacro(name);
-        }
+        String name = exp.getName();        
         result = name + "(";         
         boolean first = true;
         for (Iterator iter = exp.getParams().iterator(); iter.hasNext(); )
@@ -285,6 +185,7 @@ public class NodesToC extends NodesToJava {
 	@Override
 	public String convertType(Type type)
 	{
+		assert type instanceof TypePrimitive;
 		if(type instanceof TypePrimitive) {
 			switch(((TypePrimitive)type).getType()) {
 				case TypePrimitive.TYPE_INT8:  return "unsigned char";
@@ -292,14 +193,10 @@ public class NodesToC extends NodesToJava {
 				case TypePrimitive.TYPE_INT32: return "unsigned int";
 				case TypePrimitive.TYPE_INT64: return "unsigned long long";
 	            case TypePrimitive.TYPE_BOOLEAN:
-				case TypePrimitive.TYPE_BIT:   return "unsigned char";
+				case TypePrimitive.TYPE_BIT:   return "bitvec<1>";
 	            case TypePrimitive.TYPE_SIGINT: return "int";
 			}
-		}
-		if( type instanceof TypeArray){
-			TypeArray ta = (TypeArray) type;			
-			return convertType(ta.getBase()) + "*"; 
-		}
+		}		
 		return super.convertType(type);
 	}
 
