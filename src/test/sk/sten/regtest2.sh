996e9530dc12781b034062fc7fee21dcc0059f6f
asolar <none@none>
2007-08-30 15:06:25 -0700

    don't know what these are, will commit anyway.

---
+++ src/test/sk/sten/regtest2.sh
@@ -0,0 +1,28 @@
+for x in `ls *.sk`
+do 
+
+bname=`echo  $x | sed 's/\.sk//'`
+
+rm -f ${bname}*.c
+rm -f ${bname}
+rm -f ${bname}*.h
+rm -f script
+
+
+
+echo "RUNNING $x  $d";
+bash preproc.sh --dovectorization --outputcfiles --outputscript --outputtest --incremental 6 --seed 10 ${x} &> ${x}.output ;
+
+bash script > ${x}.sout ;
+
+
+done;
+(grep -slr 'passed' *.sout | sort > tmptmp1);
+(ls *.sout | sort >> tmptmp2); 
+echo "THE FOLLOWING TESTS FAILED:"; 
+(if (diff tmptmp1 tmptmp2) then (echo "ALL TESTS PASSED") else (echo "END") fi ); 
+rm tmptmp1 tmptmp2;
+rm *.tmp;
+
+
+
