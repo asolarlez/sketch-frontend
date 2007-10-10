c716af045ecaf4976dd1383e63f715e83436dd83
asolar <none@none>
2007-10-09 17:52:08 -0700

    Created new main method to do spin conversion.

---
+++ src/main/java/sketch/compiler/main/par/old/ToSpin.java
@@ -0,0 +1,45 @@
+package streamit.frontend;
+
+import streamit.frontend.nodes.Program;
+import streamit.frontend.passes.ConstantReplacer;
+import streamit.frontend.passes.SpinPreprocessor;
+import streamit.frontend.stencilSK.StaticHoleTracker;
+import streamit.frontend.tosbit.ValueOracle;
+
+public class ToSpin extends ToSBit {
+
+	
+	public void run()
+	{
+		parseProgram();
+
+		prog = (Program)prog.accept(new ConstantReplacer(params.varValues("D")));
+		//dump (prog, "After replacing constants:");
+		//if (!SemanticChecker.check(prog))
+		//	throw new IllegalStateException("Semantic check failed");
+
+		prog=preprocessProgram(prog); // perform prereq transformations
+		//prog.accept(new SimpleCodePrinter());
+		// RenameBitVars is buggy!! prog = (Program)prog.accept(new RenameBitVars());
+		//if (!SemanticChecker.check(prog))
+		//	throw new IllegalStateException("Semantic check failed");
+		
+		prog = (Program)prog.accept(new SpinPreprocessor(varGen));
+		
+		dump (prog, "After everything constants:");
+		
+		
+		System.out.println("DONE");
+	}
+	
+	
+	protected ToSpin(String[] args){
+		super(args);		
+	}
+	
+	public static void main(String[] args)
+	{
+		new ToSpin(args).run();
+		System.exit(0);
+	}
+}
