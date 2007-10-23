f50bd6897358520e8cad6cfdf755c7fde33d31ae
asolar <none@none>
2007-10-23 14:49:56 -0700

    Added new parameters; debug ouptut.

--- src/main/java/sketch/compiler/main/par/old/ToPSbit.java
+++ src/main/java/sketch/compiler/main/par/old/ToPSbit.java
@@ -4,6 +4,7 @@ import streamit.frontend.CommandLineParamManager.POpts;
 import streamit.frontend.experimental.simplifier.ScalarizeVectorAssignments;
 import streamit.frontend.nodes.MakeBodiesBlocks;
 import streamit.frontend.nodes.Program;
+import streamit.frontend.parallelEncoder.LockPreprocessing;
 import streamit.frontend.parallelEncoder.ProduceParallelModel;
 import streamit.frontend.passes.ConstantReplacer;
 import streamit.frontend.passes.DisambiguateUnaries;
@@ -40,7 +41,7 @@ public class ToPSbit extends ToSBit {
 		prog = (Program)prog.accept(new EliminateStructs(varGen));
 		prog = (Program)prog.accept(new DisambiguateUnaries(varGen));
 		//dump (prog, "After eliminating structs:");
-		ProduceParallelModel ppm = new ProduceParallelModel(this.varGen, params.flagValue("schedlen"));
+		ProduceParallelModel ppm = new ProduceParallelModel(this.varGen, params.flagValue("schedlen"), params.flagValue("locklen") );
 		prog = (Program) prog.accept(ppm);
 		dump (prog, "After producing parallel model:");
 		prog = (Program)prog.accept(new EliminateMultiDimArrays());
@@ -48,20 +49,22 @@ public class ToPSbit extends ToSBit {
 		prog = (Program)prog.accept(new ExtractRightShifts(varGen));
 		prog = (Program)prog.accept(new ExtractVectorsInCasts(varGen));
 		prog = (Program)prog.accept(new SeparateInitializers());
-		//dump (prog, "SeparateInitializers:");
+		dump (prog, "SeparateInitializers:");
 		//prog = (Program)prog.accept(new NoRefTypes());
 		prog = (Program)prog.accept(new ScalarizeVectorAssignments(varGen));
 		if( params.hasFlag("showpartial")  ) prog.accept(new SimpleCodePrinter());
 
 		prog = (Program)prog.accept(new EliminateNestedArrAcc());
-		//dump (prog, "After lowerIR:");
+		dump (prog, "After lowerIR:");
 	}
 	
 	
 	public void run()
 	{
 		parseProgram();
-
+		
+		prog = (Program)prog.accept(new LockPreprocessing());
+		dump(prog, "first");
 		prog = (Program)prog.accept(new ConstantReplacer(params.varValues("D")));
 		//dump (prog, "After replacing constants:");
 		if (!SemanticChecker.check(prog))
@@ -91,6 +94,15 @@ public class ToPSbit extends ToSBit {
 		params.setAllowedParam("schedlen", new POpts(POpts.NUMBER,
 				"--schedlen  n \t Sets the length of the schedule for the parallel sections to n.",
 				"10", null) );
+		
+		params.setAllowedParam("locklen", new POpts(POpts.NUMBER,
+				"--locklen  n \t This is another one of those parameters that have to do with the way" +
+				"             \t things are implemented. The locks array has to be of a static size. " +
+				"             \t When you lock on a pointer, the pointer is transformed based on some " +
+				"             \t strange function, and the resulting value is used to index the lock array. " +
+				"             \t If that index is out of bounds, your sketch will not resolve, so you use this " +
+				"             \t parameter to make that lock array larger.",
+				"50", null) );
 	}
 	
 	
