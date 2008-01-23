cf02902ae9b0cea77e3a533d08a6abe26d98747c
asolar <none@none>
2008-01-23 13:30:06 -0800

    Modified to support parallel sections. Parallel sections are handled conservatively. Information about shared variables is propagated conservatively.

--- src/main/java/sketch/compiler/dataflow/abstractValue.java
+++ src/main/java/sketch/compiler/dataflow/abstractValue.java
@@ -1,9 +1,20 @@
 package streamit.frontend.experimental;
 
 import java.util.List;
-
+/**
+ * 
+ * This is a value for abstract interpretation. 
+ * 
+ * @author asolar
+ *
+ */
 public abstract class  abstractValue{
-
+	protected boolean isVolatile = false;
+	public boolean isVolatile(){ return isVolatile; }
+	public void makeVolatile(){ isVolatile = true; }
+	public void nonVolatile(){ isVolatile = false; }
+	
+	
 	abstract public boolean isVect();
 	abstract public List<abstractValue> getVectValue();
 	abstract public boolean isBottom();
