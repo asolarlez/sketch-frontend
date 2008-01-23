ac2ae8fa1dccec54f4780cd4bb7477adf5811fea
asolar <none@none>
2008-01-23 13:28:25 -0800

    New class performs synthesis verification loop at the AST level.

---
+++ src/main/java/sketch/compiler/main/par/ParallelSketchMain.java
@@ -0,0 +1,109 @@
+package streamit.frontend;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Map;
+
+import streamit.frontend.nodes.Program;
+import streamit.frontend.nodes.Statement;
+import streamit.frontend.passes.ConstantReplacer;
+import streamit.frontend.passes.SemanticChecker;
+import streamit.frontend.solvers.Synthesizer;
+import streamit.frontend.solvers.Verifier;
+import streamit.frontend.tosbit.ValueOracle;
+
+
+
+public class ToPSbitII extends ToSBit {
+
+
+	
+	
+	public Synthesizer createSynth(Program p){
+		return null;
+	}
+	
+	public Verifier createVerif(Program p){
+		return null;
+	}
+	
+	public ValueOracle randomOracle(Program p){
+		return null;
+	}
+	
+	public void synthVerifyLoop(){
+		lowerIRToJava();
+	
+		Synthesizer synth = createSynth(prog);
+		Verifier verif = createVerif(prog);
+		
+		ValueOracle ora = randomOracle(prog);
+		boolean success = false;
+		do{
+			
+			Program cex = verif.verify( ora );
+			if(cex == null){
+				success = true;
+				break;
+				//we are done;
+			}
+			
+			ora = synth.nextCandidate(cex);
+			if(ora == null){
+				success = false;
+				break;
+			}
+		}while(true);
+		
+		oracle = ora;
+		
+	}
+	
+	
+	public void run()
+	{
+		parseProgram();
+
+		prog = (Program)prog.accept(new ConstantReplacer(params.varValues("D")));
+		//dump (prog, "After replacing constants:");
+		if (!SemanticChecker.check(prog))
+			throw new IllegalStateException("Semantic check failed");
+
+		prog=preprocessProgram(prog); // perform prereq transformations
+		//prog.accept(new SimpleCodePrinter());
+		// RenameBitVars is buggy!! prog = (Program)prog.accept(new RenameBitVars());
+		// if (!SemanticChecker.check(prog))
+		//	throw new IllegalStateException("Semantic check failed");
+		
+		if (prog == null)
+			throw new IllegalStateException();
+
+		
+		synthVerifyLoop();
+						
+		
+		eliminateStar();
+		generateCode();
+		System.out.println("DONE");
+
+	}
+	
+	
+	public ToPSbitII(String[] args){
+		super(args);
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		new ToPSbitII (args).run();
+		System.exit(0);
+	}
+}
