6c016288f04f1336a6d90992a8ab3e4f83d08e67
cgjones <none@none>
2008-02-28 18:06:55 -0800

    First (partially) working version of PSketchII.  Changes include:
      * new code printer that observes the visitor pattern
      * rewritten promela code generator, that uses the above
      * various bug fixes and added convenience methods/classes

---
+++ src/main/java/sketch/util/NullStream.java
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+package streamit.misc;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * An output stream that is functionally equivalent to /dev/null.
+ *
+ * @author <a href="mailto:cgjones@cs.berkeley.edu">Chris Jones</a>
+ */
+public class NullStream extends OutputStream {
+	public static final NullStream INSTANCE = new NullStream ();
+
+	public NullStream () { }
+
+	public void close () throws IOException { }
+	public void flush () throws IOException { }
+	public void write (byte[] b) throws IOException { }
+	public void write (byte[] b, int off, int len) throws IOException { }
+	public void write (int b) throws IOException { }
+}
