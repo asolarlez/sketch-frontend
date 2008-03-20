47c7b2f79f327cdaddf64b5add8927d39e53bb76
asolar <none@none>
2008-03-19 22:58:44 -0700

    *** empty log message ***

---
+++ src/main/java/sketch/compiler/passes/lowering/AddLastAssignmentToFork.java
@@ -0,0 +1,41 @@
+package streamit.frontend.passes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import streamit.frontend.nodes.ExprConstInt;
+import streamit.frontend.nodes.ExprVar;
+import streamit.frontend.nodes.FEReplacer;
+import streamit.frontend.nodes.Statement;
+import streamit.frontend.nodes.StmtAssign;
+import streamit.frontend.nodes.StmtBlock;
+import streamit.frontend.nodes.StmtFork;
+import streamit.frontend.nodes.StmtVarDecl;
+import streamit.frontend.nodes.TypePrimitive;
+
+public class AddLastAssignmentToFork extends FEReplacer {
+
+	
+	@Override
+	public Object visitStmtFork(StmtFork stmt){
+		
+		Statement svd = new StmtVarDecl(stmt, TypePrimitive.bittype, "_END_", ExprConstInt.zero);
+		
+		Statement fas = new StmtAssign(new ExprVar(stmt, "_END_"), ExprConstInt.zero);
+		
+		List<Statement> blist = new ArrayList<Statement>();
+		blist.add(svd);
+		blist.add(stmt.getBody());
+		blist.add(fas);
+		
+		 
+		
+		StmtFork nfork = new StmtFork(stmt, stmt.getLoopVarDecl(), stmt.getIter(), new StmtBlock(stmt.getBody(), blist)); 
+		return nfork;		
+	}
+	
+	
+	
+	
+	
+}
