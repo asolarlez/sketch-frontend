acf4bcba06d0a2002db7f858c659f5f103293f62
cgjones <none@none>
2008-03-19 18:24:45 -0700

    Three changes:
    1. Fixed bug with 'reorder' and 'insert' elimination.  This fix slightly changes the semantics of 'insert', but the change is for the better IMO.
    
    2. Added the first "null-hypothesis" solver, which uses a synthesizer that simply returns a random candidate program.
    
    3. Modified ProtectArrayAccesses to accept a 'FailurePolicy' that controls what happens when array accesses are out-of-bounds.  The two current policies are (i) fail silently for writes, and return 0 for reads; and (ii) cause an assertion failure.  The former policy follows SKETCH semantics, but the latter seems to make more sense.  In addition, it allowed parTest/regtest/miniTest16.sk to synthesize with fewer iterations.  The 'ASSERTION' policy is now default in PSketchII.

--- src/main/java/sketch/compiler/passes/lowering/EliminateInsertBlocks.java
+++ src/main/java/sketch/compiler/passes/lowering/EliminateInsertBlocks.java
@@ -14,7 +14,6 @@ import streamit.frontend.nodes.ExprVar;
 import streamit.frontend.nodes.FEReplacer;
 import streamit.frontend.nodes.Statement;
 import streamit.frontend.nodes.StmtAssert;
-import streamit.frontend.nodes.StmtAssign;
 import streamit.frontend.nodes.StmtBlock;
 import streamit.frontend.nodes.StmtIfThen;
 import streamit.frontend.nodes.StmtInsertBlock;
@@ -40,25 +39,49 @@ import streamit.frontend.nodes.TypePrimitive;
  *   sn;
  *   if (n+1 == where)  S;
  *
+ * There is a special case for nested 'insert' blocks:
+ *
+ *   insert S1 into { insert S2 into { ... } }
+ *
  * @author <a href="mailto:cgjones@cs.berkeley.edu">Chris Jones</a>
  */
 public class EliminateInsertBlocks extends FEReplacer {
 	protected TempVarGen varGen;
+	/**
+	 * The 'depth' and 'header' variables are a bit of a hack.  When
+	 * rewriting nested 'insert' blocks, we need to introduce two new
+	 * statements.  However, these statements shouldn't be part of the
+	 * block in which a stmt is inserted.  So all these introduced stmts are
+	 * collected into 'header' and only emitted when 'depth' again reaches 0.
+	 */
+	protected int depth;
+	protected List<Statement> header;
 
 	public EliminateInsertBlocks (TempVarGen _varGen) {
 		varGen = _varGen;
 	}
 
 	public Object visitStmtInsertBlock (StmtInsertBlock sib) {
+		if (0 == depth++)
+			header = new ArrayList<Statement> ();
+
 		String where = varGen.nextVar ("_ins_where");
 		Statement S = (Statement) sib.getInsertStmt ().accept (this);
-		List<Statement> oldB = sib.getIntoBlock ().getStmts ();
-		List<Statement> newB = new ArrayList<Statement> ();
-		String maxVal = ""+ oldB.size ();
 
-		addStatement (new StmtVarDecl (sib, TypePrimitive.inttype, where, null));
-		addStatement (new StmtAssign (new ExprVar (sib, where), new ExprStar (sib)));
-		addStatement (new StmtAssert (sib,
+		// Rewrite 'into' body
+		List<Statement> oldB = sib.getIntoBlock ().getStmts ();
+		if (1 == oldB.size () && (oldB.get (0) instanceof StmtInsertBlock))
+			// Special case for immediately-nested 'insert' blocks
+			oldB = ((StmtBlock) oldB.get (0).accept (this)).getStmts ();
+		else
+			oldB = ((StmtBlock) sib.getIntoBlock ().accept (this)).getStmts ();
+
+		String maxVal = ""+ oldB.size ();
+		int nBits = 32 - Integer.numberOfLeadingZeros(oldB.size () - 1); // ceiling (lg (size))
+		nBits = nBits > 0 ? nBits : 1;	// lg(1) is a special case
+
+		header.add (0,
+			new StmtAssert (sib,
 				new ExprBinary (
 						new ExprBinary (ExprConstInt.zero, "<=",
 										new ExprVar (sib, where)),
@@ -66,9 +89,12 @@ public class EliminateInsertBlocks extends FEReplacer {
 						new ExprBinary (new ExprVar (sib, where), "<=",
 										ExprConstant.createConstant (sib, maxVal))
 								)));
+		header.add (0,
+				new StmtVarDecl (sib, TypePrimitive.inttype, where, new ExprStar (sib, nBits)));
 
+		List<Statement> newB = new ArrayList<Statement> ();
 		for (int i = 0; i < oldB.size (); ++i) {
-			Statement si = (Statement) oldB.get (i).accept (this);
+			Statement si = (Statement) oldB.get (i);
 
 			newB.add (new StmtIfThen (si,
 						new ExprBinary (new ExprVar (si, where),
@@ -80,6 +106,9 @@ public class EliminateInsertBlocks extends FEReplacer {
 					new ExprBinary (new ExprVar (sib, where),
 								    "==", ExprConstant.createConstant (sib, maxVal)),
 					S, null));
+
+		if (0 == --depth)
+			newB.addAll (0, header);
 
 		return new StmtBlock (sib, newB);
 	}
