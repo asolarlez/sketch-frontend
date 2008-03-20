740c61a102ed3e79d1b3528c5585f3467754c30f
cgjones <none@none>
2008-03-19 18:24:45 -0700

    Three changes:
    1. Fixed bug with 'reorder' and 'insert' elimination.  This fix slightly changes the semantics of 'insert', but the change is for the better IMO.
    
    2. Added the first "null-hypothesis" solver, which uses a synthesizer that simply returns a random candidate program.
    
    3. Modified ProtectArrayAccesses to accept a 'FailurePolicy' that controls what happens when array accesses are out-of-bounds.  The two current policies are (i) fail silently for writes, and return 0 for reads; and (ii) cause an assertion failure.  The former policy follows SKETCH semantics, but the latter seems to make more sense.  In addition, it allowed parTest/regtest/miniTest16.sk to synthesize with fewer iterations.  The 'ASSERTION' policy is now default in PSketchII.

---
+++ src/main/java/sketch/compiler/solvers/RandomSynthesizer.java
@@ -0,0 +1,37 @@
+/**
+ *
+ */
+package streamit.frontend.solvers;
+
+import streamit.frontend.CommandLineParamManager;
+import streamit.frontend.nodes.TempVarGen;
+import streamit.frontend.stencilSK.StaticHoleTracker;
+import streamit.frontend.tosbit.RandomValueOracle;
+import streamit.frontend.tosbit.ValueOracle;
+
+/**
+ * A synthesizer that uar picks a new candidate program.  There is no guarantee
+ * that each candidate will be unique.
+ *
+ * This should never be used in a real system; it is for testing only.
+ *
+ * @author <a href="mailto:cgjones@cs.berkeley.edu">Chris Jones</a>
+ */
+public class RandomSynthesizer implements Synthesizer {
+	protected CommandLineParamManager params;
+	protected TempVarGen varGen;
+
+	public RandomSynthesizer (CommandLineParamManager _params, TempVarGen _varGen) {
+		params = _params;
+		varGen = _varGen;
+	}
+
+	public ValueOracle nextCandidate (CounterExample cex) {
+		return new RandomValueOracle (new StaticHoleTracker(varGen));
+	}
+
+	/** This method assumes that it takes 0 time to create a random solution. */
+	public SolutionStatistics getLastSolutionStats () {
+		return new SATSolutionStatistics ();
+	}
+}
