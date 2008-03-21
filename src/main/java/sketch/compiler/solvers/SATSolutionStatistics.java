8f861267a9e37bd96c4ba7f559990769b416c896
cgjones <none@none>
2008-03-20 21:17:32 -0700

    Grabbed a few more stats from the backend (problem sizes, controls).
    Also added support for queue benchmarks parameterized by the number of threads and operations.

--- src/main/java/sketch/compiler/solvers/SATSolutionStatistics.java
+++ src/main/java/sketch/compiler/solvers/SATSolutionStatistics.java
@@ -15,9 +15,23 @@ public class SATSolutionStatistics extends SolutionStatistics {
 	protected long solutionTimeMs;
 	protected long maxMemUsageBytes;
 
+	protected long numNodesInitial;
+	protected long numNodesFinal;
+	protected long numControls;
+	protected long numControlBits;
+
 	public long elapsedTimeMs () 	   {  return elapsedTimeMs;  }
 	public long maxMemoryUsageBytes () {  return maxMemUsageBytes;  }
 	public long modelBuildingTimeMs () {  return modelBuildingTimeMs;  }
 	public long solutionTimeMs ()      {  return solutionTimeMs;  }
 	public boolean successful () 	   {  return success;  }
+
+	public String toString () {
+		return super.toString () +
+"      [SAT-specific solution stats]\n"+
+"      initial number of nodes ---------> "+ numNodesInitial +"\n"+
+"      number of nodes after opts ------> "+ numNodesFinal +"\n"+
+"      number of controls --------------> "+ numControls +"\n"+
+"      total number of control bits ----> "+ numControlBits +"\n";
+	}
 }
