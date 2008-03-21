9de0d2802a64b95ee0a190ec5132cfe500514935
cgjones <none@none>
2008-03-20 20:12:17 -0700

    Two changes:
      * added the ability to switch between the exponential and quadratic 'reorder' encodings
      * passed the randomness seed to the initial control values in PSbitII

--- src/main/java/sketch/compiler/passes/lowering/EliminateReorderBlocks.java
+++ src/main/java/sketch/compiler/passes/lowering/EliminateReorderBlocks.java
@@ -1,6 +1,7 @@
 package streamit.frontend.passes;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -13,6 +14,7 @@ import streamit.frontend.nodes.ExprVar;
 import streamit.frontend.nodes.Expression;
 import streamit.frontend.nodes.FEReplacer;
 import streamit.frontend.nodes.Statement;
+import streamit.frontend.nodes.StmtInsertBlock;
 import streamit.frontend.nodes.StmtReorderBlock;
 import streamit.frontend.nodes.StmtAssert;
 import streamit.frontend.nodes.StmtAssign;
@@ -33,10 +35,57 @@ import streamit.frontend.nodes.TypePrimitive;
  */
 public class EliminateReorderBlocks extends FEReplacer {
 
+	private boolean insertBlockRewrite;
 	private TempVarGen varGen;
 
 	public EliminateReorderBlocks(TempVarGen varGen){
+		this (varGen, false);
+	}
+
+	public EliminateReorderBlocks (TempVarGen varGen, boolean insertBlockRewrite) {
 		this.varGen = varGen;
+		this.insertBlockRewrite = insertBlockRewrite;
+	}
+
+	public Object visitStmtReorderBlock (StmtReorderBlock stmt) {
+		return insertBlockRewrite ? lowerToInsertBlocks (stmt)
+				: lowerToSwitchedLoop (stmt);
+	}
+
+	protected Statement lowerToInsertBlocks (StmtReorderBlock srb) {
+		List<Statement> B = srb.getStmts ();
+
+		if (1 >= B.size ())
+			return (Statement) srb.getBlock ().accept (this);	// no reorder for 1 stmt
+
+		StmtBlock into = new StmtBlock (srb,
+				Collections.singletonList ((Statement)
+						(new StmtReorderBlock (srb, B.subList (1, B.size ()))).accept (this)));
+		Statement insert = (Statement) B.get (0).accept (this);
+
+		return new StmtInsertBlock (srb, insert, into);
+	}
+
+	public Statement lowerToSwitchedLoop (StmtReorderBlock stmt) {
+		Iterator<Statement> iter = stmt.getStmts().iterator();
+		String name = varGen.nextVar();
+		int len = stmt.getStmts().size();
+		Statement s = recursiveCondGenerator(iter, name, 0);
+		if(s==null) return null;
+		assert len > 0;
+		Expression elen = new ExprConstInt(len);
+		StmtVarDecl svd = new StmtVarDecl(s, new TypeArray(TypePrimitive.bittype, elen), name, ExprConstInt.zero );
+		StmtLoop sloop = new StmtLoop(s, elen, s);
+		Expression var = new ExprVar(s, name);
+		Expression fex = new ExprArrayRange(s, var, ExprConstInt.zero);
+		for(int i=1; i<len; ++i){
+			fex = new ExprBinary(s, ExprBinary.BINOP_AND,  fex  , new ExprArrayRange(s, var, new ExprConstInt(i)));
+		}
+		List<Statement> slist = new ArrayList<Statement>(2);
+		slist.add(svd);
+		slist.add(sloop);
+		slist.add(new StmtAssert(s, fex));
+		return new StmtBlock(s, slist);
 	}
 
 	private Statement recursiveCondGenerator(Iterator<Statement> iter, String iname, int i){
@@ -58,27 +107,4 @@ public class EliminateReorderBlocks extends FEReplacer {
 			return new StmtIfThen(s, cond, new StmtBlock(s, slist), elsebranch);
 		}
 	}
-
-	 public Object visitStmtReorderBlock(StmtReorderBlock stmt){
-
-		 Iterator<Statement> iter = stmt.getStmts().iterator();
-		 String name = varGen.nextVar();
-		 int len = stmt.getStmts().size();
-		 Statement s = recursiveCondGenerator(iter, name, 0);
-		 if(s==null) return null;
-		 assert len > 0;
-		 Expression elen = new ExprConstInt(len);
-		 StmtVarDecl svd = new StmtVarDecl(s, new TypeArray(TypePrimitive.bittype, elen), name, ExprConstInt.zero );
-		 StmtLoop sloop = new StmtLoop(s, elen, s);
-		 Expression var = new ExprVar(s, name);
-		 Expression fex = new ExprArrayRange(s, var, ExprConstInt.zero);
-		 for(int i=1; i<len; ++i){
-			 fex = new ExprBinary(s, ExprBinary.BINOP_AND,  fex  , new ExprArrayRange(s, var, new ExprConstInt(i)));
-		 }
-		 List<Statement> slist = new ArrayList<Statement>(2);
-		 slist.add(svd);
-		 slist.add(sloop);
-		 slist.add(new StmtAssert(s, fex));
-		 return new StmtBlock(s, slist);
-    }
 }
