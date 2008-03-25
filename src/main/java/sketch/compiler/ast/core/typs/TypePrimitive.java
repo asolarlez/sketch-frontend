381c3a277fb89fd80ccae9733d81a2d9898c125e
cgjones <none@none>
2008-03-24 17:35:43 -0700

    Fixed uninitialized variable bug created by the EliminateConditionals pass.
    Also added a new API function for 'Type's to query their default value.

--- src/main/java/sketch/compiler/ast/core/typs/TypePrimitive.java
+++ src/main/java/sketch/compiler/ast/core/typs/TypePrimitive.java
@@ -42,16 +42,16 @@ public class TypePrimitive extends Type
     /** Type constant for void types. */
     public static final int TYPE_VOID = 9;
     /** Type constant for boolean types. */
-    public static final int TYPE_BOOLEAN = 10;    
+    public static final int TYPE_BOOLEAN = 10;
     /** Type constant for signed integers. */
     public static final int TYPE_SIGINT = 14;
-    
+
     /** Type constant for signed integers. */
     public static final int TYPE_NULLPTR = 15;
-    
+
     /** For internal use only. This type can be cast to anything, and anything can be cast to it.*/
     public static final int TYPE_ANYTYPE = 16;
-    
+
     /** Type object for boolean types. */
     public static final TypePrimitive booltype =
         new TypePrimitive(TYPE_BOOLEAN);
@@ -77,15 +77,15 @@ public class TypePrimitive extends Type
         new TypePrimitive(TYPE_VOID);
     public static final TypePrimitive nulltype =
         new TypePrimitive(TYPE_NULLPTR);
-    
+
     public static final TypePrimitive anytype =
         new TypePrimitive(TYPE_ANYTYPE);
-    
+
     private int type;
 
     /**
-     * Create a new primitive type. It's private, because we 
-     * don't want other classes creating their own primitive types. 
+     * Create a new primitive type. It's private, because we
+     * don't want other classes creating their own primitive types.
      * They should use only the ones we have defined statically above.
      *
      * This allows us to compare primitive types with simple pointer equality, without having to call the equals operator.
@@ -95,7 +95,7 @@ public class TypePrimitive extends Type
     {
         this.type = type;
     }
-    
+
     /**
      * Get the type number for this type.
      *
@@ -140,14 +140,14 @@ public class TypePrimitive extends Type
 
         case TYPE_ANYTYPE:
         	return "any";
-        	
+
         case TYPE_SIGINT:
         	return "int";
         default:
             return "<primitive type " + type + ">";
         }
     }
-    
+
     /**
      * Check if this type can be promoted to some other type.
      * Returns true if a value of this type can be assigned to
@@ -165,19 +165,19 @@ public class TypePrimitive extends Type
         	if(that instanceof TypeArray){
         		return this.promotesTo(((TypeArray)that).getBase());
         	}else{
-        		if(this.type == TYPE_NULLPTR && (that instanceof TypeStructRef || that instanceof TypeStruct ) ) 
+        		if(this.type == TYPE_NULLPTR && (that instanceof TypeStructRef || that instanceof TypeStruct ) )
         			return true;
-        		
+
         		return false;
         	}
         }
-            
+
 
         int t1 = this.type;
         int t2 = ((TypePrimitive)that).type;
-        
+
         if(t2 == TYPE_ANYTYPE) return true;
-        
+
         // want: "t1 < t2", more or less
         switch(t1)
         {
@@ -187,7 +187,7 @@ public class TypePrimitive extends Type
                 t2 == TYPE_COMPLEX ;
         case TYPE_BIT:
             return t2 == TYPE_SIGINT || t2 == TYPE_BIT || t2 == TYPE_INT ||
-                t2 == TYPE_FLOAT || t2 == TYPE_COMPLEX ;            
+                t2 == TYPE_FLOAT || t2 == TYPE_COMPLEX ;
         case TYPE_INT:
             return t2 == TYPE_SIGINT || t2 == TYPE_INT || t2 == TYPE_FLOAT || t2 == TYPE_COMPLEX ;
         case TYPE_SIGINT:
@@ -196,8 +196,8 @@ public class TypePrimitive extends Type
             return t2 == TYPE_FLOAT || t2 == TYPE_COMPLEX;
         case TYPE_COMPLEX:
             return t2 == TYPE_COMPLEX;
-       
-        case TYPE_INT16:         	
+
+        case TYPE_INT16:
         case TYPE_INT8:
         case TYPE_INT64:
         	return t2 == TYPE_SIGINT || t2 == TYPE_INT || t2 == TYPE_FLOAT || t2 == TYPE_COMPLEX ;
@@ -228,13 +228,36 @@ public class TypePrimitive extends Type
             return false;
         return true;
     }
-    
+
+    public Expression defaultValue () {
+    	switch (type) {
+    	case TYPE_BOOLEAN:
+    		return ExprConstBoolean.FALSE;
+    	case TYPE_BIT:
+    	case TYPE_INT8:  case TYPE_INT16:  case TYPE_INT32:  case TYPE_INT64:
+    	/*case TYPE_INT:*/   case TYPE_SIGINT:
+    		return ExprConstInt.zero;
+    	case TYPE_FLOAT: case TYPE_DOUBLE:
+        	return ExprConstFloat.ZERO;
+    	case TYPE_COMPLEX:
+    		return ExprComplex.ZERO;
+    	case TYPE_NULLPTR:
+        	return ExprNullPtr.nullPtr;
+    	case TYPE_VOID:
+    	case TYPE_ANYTYPE:
+    		assert false : "Type "+ type +" doesn't have a default value.";
+    	default:
+    		assert false : "Unknown type "+ type +".";
+    		return null;	// unreachable
+    	}
+    }
+
     public int hashCode()
     {
         return new Integer(type).hashCode();
     }
-    
-    
+
+
     public Object accept(FEVisitor visitor){
     	return visitor.visitTypePrimitive(this);
     }
