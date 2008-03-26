228334872bbf964101d403faf502db8ce5cc38d0
cgjones <none@none>
2008-03-25 23:08:05 -0700

    Make a hack less hacky, and make debugging more pleasant.

---
+++ src/main/java/sketch/util/ControlFlowException.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package streamit.misc;
+
+/**
+ * A bastard exception that represents not an error, but a desire to bypass
+ * the normal control flow.
+ *
+ * This can be used to, e.g., break out of a deep  'visitor'
+ * traversal of an AST when some property of interest is found to hold true.
+ *
+ * @author <a href="mailto:cgjones@cs.berkeley.edu">Chris Jones</a>
+ */
+public class ControlFlowException extends RuntimeException {
+	public static final long serialVersionUID = -1;		// not serializable
+
+	public ControlFlowException (String tag) {
+		super (tag);
+	}
+
+	public String getTag () {  return getMessage ();  }
+}
