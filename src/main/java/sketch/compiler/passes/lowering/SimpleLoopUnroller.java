c26d787b51faebee177939502503d7650a0b87d9
asolar <none@none>
2008-03-25 19:39:16 -0700

    *** empty log message ***

--- src/main/java/sketch/compiler/passes/lowering/SimpleLoopUnroller.java
+++ src/main/java/sketch/compiler/passes/lowering/SimpleLoopUnroller.java
@@ -14,37 +14,38 @@ import streamit.frontend.stencilSK.VarReplacer;
 
 public class SimpleLoopUnroller extends FEReplacer {
 
-	@Override
-	public Object visitStmtFor(StmtFor stmt)
-    {
+	
+	
+	
+	public int decideForLoop(StmtFor stmt){
 		if(!(stmt.getInit() instanceof StmtVarDecl)){
-			return super.visitStmtFor(stmt);
+			return -1;
 		}
 		StmtVarDecl svd = (StmtVarDecl)stmt.getInit();
 		if(svd.getNumVars() != 1){
-			return super.visitStmtFor(stmt);
+			return -1;
 		}
 		Expression init = svd.getInit (0);
 		stmt.assertTrue (null != init,
 				"need an initializer for loop var '"+ svd.getName (0) +"'");
 		Integer low = svd.getInit(0).getIValue();
 		if(low == null){
-			return super.visitStmtFor(stmt);
+			return -1;
 		}
 		String indName = svd.getName(0);
 		if(!( stmt.getCond() instanceof ExprBinary )  ){
-			return super.visitStmtFor(stmt);
+			return -1;
 		}
 		ExprBinary eb = (ExprBinary) stmt.getCond();
 		if(eb.getOp() != ExprBinary.BINOP_LE && eb.getOp() != ExprBinary.BINOP_LT){
-			return super.visitStmtFor(stmt);
+			return -1;
 		}
 		if( ! eb.getLeft().toString().equals(indName)  ){
-			return super.visitStmtFor(stmt);
+			return -1;
 		}
 		Integer high = eb.getRight().getIValue();
 		if(high == null){
-			return super.visitStmtFor(stmt);
+			return -1;
 		}
 		int sz = high - low;
 		if(  eb.getOp() == ExprBinary.BINOP_LE ){
@@ -54,17 +55,17 @@ public class SimpleLoopUnroller extends FEReplacer {
 		if(stmt.getIncr() instanceof StmtAssign){
 			StmtAssign sa = (StmtAssign)stmt.getIncr();
 			if(!sa.getLHS().toString().equals(indName)){
-				return super.visitStmtFor(stmt);
+				return -1;
 			}
 
 			if(!(sa.getRHS() instanceof ExprBinary)){
-				return super.visitStmtFor(stmt);
+				return -1;
 			}
 			ExprBinary rhsbin = (ExprBinary) sa.getRHS();
 
 			Integer rhsrhs = rhsbin.getRight().getIValue();
 			if(rhsbin.getOp() != ExprBinary.BINOP_ADD || rhsrhs == null || rhsrhs != 1 || !rhsbin.getLeft().toString().equals(indName)){
-				return super.visitStmtFor(stmt);
+				return -1;
 			}
 		}else{
 			if(stmt.getIncr() instanceof StmtExpr){
@@ -74,14 +75,41 @@ public class SimpleLoopUnroller extends FEReplacer {
 								||((ExprUnary)se.getExpression()).getOp() == ExprUnary.UNOP_PREINC )){
 
 				}else{
-					return super.visitStmtFor(stmt);
+					return -1;
 				}
 			}else{
-				return super.visitStmtFor(stmt);
+				return -1;
 			}
 		}
+		
+		return sz;
 
-
+	}
+	
+	public boolean unrollThisLoop(StmtFor stmt){
+		return true;
+	}
+	
+	
+	@Override
+	public Object visitStmtFor(StmtFor stmt)
+    {
+		int sz = decideForLoop(stmt);
+		
+		if(sz < 0){
+			return super.visitStmtFor(stmt);
+		}
+		
+		StmtVarDecl svd = (StmtVarDecl)stmt.getInit();
+		String indName = svd.getName(0);
+		Expression init = svd.getInit (0);
+		Integer low = svd.getInit(0).getIValue();
+		
+		if(!unrollThisLoop(stmt)){
+			return super.visitStmtFor(stmt);
+		}
+		
+		
 		for(int i=0; i<sz; ++i){
 			VarReplacer vr = new VarReplacer(indName, new ExprConstInt(i + low) );
 			Statement s = (Statement) stmt.getBody().accept(vr);
