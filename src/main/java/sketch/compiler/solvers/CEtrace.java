017944b1110eec628bf0604b3093291baf6b8fec
cgjones <none@none>
2008-05-07 15:56:21 -0700

    The verifier will now report that states at which threads were stuck when deadlock occurs.

--- src/main/java/sketch/compiler/solvers/CEtrace.java
+++ src/main/java/sketch/compiler/solvers/CEtrace.java
@@ -17,21 +17,31 @@ public class CEtrace extends CounterExample {
 	}
 
 	public List<step> steps;
+	public List<step> blockedSteps;
 
 	public CEtrace () {
 		steps = new ArrayList<step> ();
+		blockedSteps = new ArrayList<step> ();
 	}
 
 	public void addStep (int thread, int stmt) {
 		steps.add (new step (thread, stmt));
 	}
 
+	public void addBlockedStep (int thread, int stmt) {
+		blockedSteps.add (new step (thread, stmt));
+	}
+
 	public void addSteps (List<step> _steps) {
 		steps.addAll (_steps);
 	}
 
+	public void addBlockedSteps (List<step> _steps) {
+		blockedSteps.addAll (_steps);
+	}
+
 	public String toString(){
-		return "(t,s)" + steps.toString();
+		return "(t,s)" + steps.toString() + "; [blocked: "+ blockedSteps.toString () + "]";
 	}
 
 	public boolean deadlocked () { return false; }
