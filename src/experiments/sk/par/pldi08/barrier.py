4229ec225431333a963e0a99258c7c739585fea2
cgjones <none@none>
2008-05-08 12:10:31 -0700

    Fixed small bug in updated handling of deadlocks.
    Also reorganized PLDI benchmarks to make performance regression tests easier to run.  The tests used for PLDI are "frozen".

---
+++ src/experiments/sk/par/pldi08/barrier.py
@@ -0,0 +1,58 @@
+import os, sys, tempfile, time
+
+import sketch
+
+# How many tests to run in parallel
+NCPUS = 1
+
+##-----------------------------------------------------------------------------
+if __name__ == '__main__':
+    print 'Renice me, please!\n'
+    print 'sudo renice -15', os.getpid ()
+    print ''
+    sys.stdout.flush ()
+
+    try:
+        time.sleep (15)
+    except KeyboardInterrupt:
+        pass
+
+    cmd = sys.argv[1:]
+    if not len (cmd):
+        cmd = ['psketch', '--heapsize', '10', '--verbosity', '2',
+                          '--timeout', '60', '--seed', '10']
+    logf = sys.stdout
+    cwd = os.getcwd ()
+    tmpfiles = []
+
+    def test (path, workdir=None, logfile=logf, extraopts=[]):
+        dir, file = os.path.split (path)
+        if not workdir:  workdir = dir
+        _, outfile = tempfile.mkstemp ('', file)
+        tmpfiles.append (outfile)
+        cmdline = cmd + extraopts + ['--output', outfile]
+        return sketch.Test (path, workdir, logfile, cmdline)
+
+    o = ['--vectorszGuess', '16384', 
+         '--reorderEncoding', 'exponential']
+
+    try:
+        tests = (
+##-----------------------------------------------------------------------------
+## Barrier
+
+test ('benchmarks/barrier/b1_3_2.sk', extraopts=o),
+test ('benchmarks/barrier/b1_3_3.sk', extraopts=o),
+test ('benchmarks/barrier/b1_4_2.sk', extraopts=o),
+test ('benchmarks/barrier/b2_3_2.sk', extraopts=o),
+test ('benchmarks/barrier/b1_4_3.sk', extraopts=o),
+
+test ('benchmarks/barrier/b1_5_2.sk', extraopts=o),
+test ('benchmarks/barrier/b2_3_3.sk', extraopts=o),
+)
+
+        sketch.prettyPrint (sketch.runTests (tests, NCPUS))
+    finally:
+        for tmpfile in tmpfiles:
+            if os.access (tmpfile, os.R_OK):         os.remove (tmpfile)
+            if os.access (tmpfile+".tmp", os.R_OK):  os.remove (tmpfile+".tmp")
