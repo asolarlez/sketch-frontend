f14e8393124bad15ba0f5886a87436fa86d16275
lshan <none@none>
2009-11-15 22:55:24 -0800

    fixed tests to work with Yices2

--- src/test/java/sketch/compiler/smt/yices/SketchRegressionTests.java
+++ src/test/java/sketch/compiler/smt/yices/SketchRegressionTests.java
@@ -5,11 +5,13 @@ import java.util.HashMap;
 
 import junit.framework.Assert;
 
+import org.junit.Test;
+
 public class SketchRegressionTests extends sketch.compiler.smt.tests.SketchRegressionTests {
 
 	protected HashMap<String, String> initCmdArgs(String input) {
 		HashMap<String, String> argsMap = super.initCmdArgs(input);
-		argsMap.put("--backend", "yices");
+		argsMap.put("--backend", "yices2");
 		
 //		 argsMap.put("--verbosity", "4");
 		// argsMap.put("--showphase", "parse");
@@ -17,37 +19,6 @@ public class SketchRegressionTests extends sketch.compiler.smt.tests.SketchRegre
 		return argsMap;
 	}
 
-	@Override
-	public void miniTest8() throws IOException, InterruptedException {
-		Assert.fail("verification takes a long time. tried running it over 1.5 hours. still not finished.");
-//		super.miniTest8();
-	}
-	
-	@Override
-	public void miniTest15() throws IOException, InterruptedException {
-		Assert.fail("yices not showing full model");
-		super.miniTest15();
-		
-		
-	}
-	
-	@Override
-	public void miniTest26() throws IOException, InterruptedException {
-		Assert.fail("yices not showing full model");
-		super.miniTest26();
-	}
-	
-	@Override
-	public void miniTest28() throws IOException, InterruptedException {
-		Assert.fail("yices not showing full model");
-		super.miniTest28();
-	}
-	
-	@Override
-	public void miniTest29() throws IOException, InterruptedException {
-		Assert.fail("yices not showing full model");
-		super.miniTest29();
-	}
 	
 	@Override
 	public void miniTest49() throws IOException, InterruptedException {
@@ -67,17 +38,6 @@ public class SketchRegressionTests extends sketch.compiler.smt.tests.SketchRegre
 		super.miniTest52();
 	}
 	
-	@Override
-	public void miniTest54() throws IOException, InterruptedException {
-		Assert.fail("BigInteger required");
-		super.miniTest54();
-	}
-	
-	@Override
-	public void miniTest85() throws IOException, InterruptedException {
-		Assert.fail("yices not showing full model");
-		super.miniTest85();
-	}
 	
 	@Override
 	public void miniTest88() throws IOException, InterruptedException {
@@ -92,15 +52,14 @@ public class SketchRegressionTests extends sketch.compiler.smt.tests.SketchRegre
 	}
 	
 	@Override
-	public void miniTestb101() throws IOException, InterruptedException {
-		Assert.fail("uninterpreted function");
-//		super.miniTestb101();
-	}
+    public void miniTest93() throws IOException, InterruptedException {
+        Assert.fail("uninterpreted function");
+        super.miniTest93();
+    }
 	
 	@Override
-	public void miniTestb120() throws IOException, InterruptedException {
-		Assert.fail("infinite loop. verifier is not finding good counterexample");
-//		super.miniTestb120();
+	public void miniTestb101() throws IOException, InterruptedException {
+		Assert.fail("uninterpreted function");
 	}
 	
 	@Override
@@ -115,21 +74,11 @@ public class SketchRegressionTests extends sketch.compiler.smt.tests.SketchRegre
 		super.miniTestb161();
 	}
 	
-	@Override
-	public void miniTest93() throws IOException, InterruptedException {
-		Assert.fail("uninterpreted function");
-		super.miniTest93();
-	}
-	
-	@Override
-	public void miniTestb105() throws IOException, InterruptedException {
-		Assert.fail("infinite CEGIS loop b/c of adverse sketch");
-		super.miniTestb105();
-	}
-	
-	@Override
-	public void miniTestb106() throws IOException, InterruptedException {
-		Assert.fail("infinite CEGIS loop b/c of adverse sketch");
-		super.miniTestb106();
-	}
+	@Test
+    public void miniTest51() throws IOException, InterruptedException {
+        HashMap<String, String> argsMap = initCmdArgsWithFileName("miniTest51.sk");
+
+        runOneTest(toArgArray(argsMap));
+    }
+
 }
