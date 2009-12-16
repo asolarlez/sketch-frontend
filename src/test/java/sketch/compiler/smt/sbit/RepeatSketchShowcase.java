4cf225f1b5bd81e91f5beca910c3a7c543533bff
lshan <none@none>
2009-12-16 09:50:31 -0800

    Added repeat tests for SBit

---
+++ src/test/java/sketch/compiler/smt/sbit/RepeatSketchShowcase.java
@@ -0,0 +1,21 @@
+package sketch.compiler.smt.sbit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses( {
+        SketchShowcase.class,
+        SketchShowcase.class,
+        SketchShowcase.class,
+        SketchShowcase.class,
+        SketchShowcase.class,
+        SketchShowcase.class,
+        SketchShowcase.class,
+        SketchShowcase.class,
+        SketchShowcase.class,
+        SketchShowcase.class,
+         })
+public class RepeatSketchShowcase {
+}
