b7d3bc6d6a90465dc0b70039173e34e577629d8e
asolar <none@none>
2010-03-14 15:29:38 -0400

    Missing class helps us rename main to _main for code generation.

---
+++ src/main/java/sketch/compiler/passes/preprocessing/MethodRename.java
@@ -0,0 +1,38 @@
+package sketch.compiler.passes.preprocessing;
+
+import java.util.Map;
+
+import sketch.compiler.ast.core.FEReplacer;
+import sketch.compiler.ast.core.Function;
+import sketch.compiler.ast.core.exprs.ExprFunCall;
+
+public class MethodRename extends FEReplacer {
+    Map<String, String> oldToNew;
+    public MethodRename(Map<String, String> oldToNew){
+        this.oldToNew = oldToNew;
+    }
+    
+    @Override
+    public Object visitFunction(Function func)
+    {
+        Function rv = (Function) super.visitFunction(func);
+        if(oldToNew.containsKey(rv.getName())){
+            return new Function(rv, rv.getCls(),
+                    oldToNew.get(rv.getName()), rv.getReturnType(),
+                    rv.getParams(), rv.getSpecification(), rv.getBody());
+        }else{
+            return rv;
+        }        
+    }
+    
+    public Object visitExprFunCall(ExprFunCall exp)
+    {
+        ExprFunCall efc = (ExprFunCall) super.visitExprFunCall(exp);
+        if(oldToNew.containsKey(efc.getName())){
+            return new ExprFunCall(efc, oldToNew.get(efc.getName()), efc.getParams());
+        }else{
+            return efc;
+        }
+    }
+    
+}
