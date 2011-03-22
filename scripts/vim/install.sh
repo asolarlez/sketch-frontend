1eb89841ba40ced7ed99c37c67de7daa98a78e48
gatoatigrado <none@none>
2011-03-22 08:13:07 -0400

    [minor] fix vim install
    --HG--
    branch : ntung-dev

--- scripts/vim/install.sh
+++ scripts/vim/install.sh
@@ -4,6 +4,7 @@ modpath="$(dirname "$(readlink -f "$0")")"
 
 set -v
 mkdir -p ~/.vim/syntax
+mkdir -p ~/.vim/ftdetect
 rm -f ~/.vim/syntax/sketch.vim
 ln -s "$modpath"/syntax/sketch.vim ~/.vim/syntax
 ln -s "$modpath"/ftdetect/sketch.vim ~/.vim/ftdetect
