9b08352237c56deb09fdd3153b12d1a266aa8ad2
Zhilei Xu <none@none>
2012-05-23 23:47:21 -0400

    spmd1d_spmd K step version. fix a bug in RemoveFunctionParameters: when the spec name is wrong, should tell the user rather than throwing nullpointerexception
    --HG--
    branch : armando_prototype

--- src/main/java/sketch/compiler/passes/preprocessing/RemoveFunctionParameters.java
+++ src/main/java/sketch/compiler/passes/preprocessing/RemoveFunctionParameters.java
@@ -394,7 +394,10 @@ public class RemoveFunctionParameters extends FEReplacer {
                     funsToVisit.add(nres.getFunName(fun.getName()));
                 }
                 if (fun.getSpecification() != null) {
-                    funsToVisit.add(nres.getFunName(fun.getSpecification()));
+                    String spec = nres.getFunName(fun.getSpecification());
+                    assert spec != null : "spec of " + fun.getName() +
+                            " is null. did you put the wrong name?";
+                    funsToVisit.add(spec);
                     funsToVisit.add(nres.getFunName(fun.getName()));
                 }
             }
