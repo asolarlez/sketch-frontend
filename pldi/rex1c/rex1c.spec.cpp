222e835a3d6e38db291734e83b14b71028e7c2ea
Zhilei Xu <none@none>
2012-10-22 16:21:36 -0500

    add some tests
    --HG--
    branch : armando_prototype

---
+++ pldi/rex1c/rex1c.spec.cpp
@@ -0,0 +1,55 @@
+#include "spmd.h"
+
+#include <fstream>
+#include <sstream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+time_t start;
+time_t end;
+time_t totalEnd;
+
+#include "rex1b.cpp"
+using namespace ANONYMOUS;
+
+int W, H;
+float * A;
+
+void init() {
+  A = new float[W*H];
+  for (int i=0; i<W; ++i) {
+    A[i] = i;
+  }
+}
+
+void output(int height, int width, float * arr) {
+  stringstream fname;
+  fname << "rex2.sout";
+  ofstream fout(fname.str().c_str());
+  for (int x=0; x<width; ++x) {
+    for (int t=0; t<height; ++t) {
+      fout << arr[width*t+x] << " ";
+    }
+    fout << endl;
+  }
+  fout.close();
+}
+
+int main(int argc, char ** argv) {
+  if (argc<3) {
+    cerr << "Usage: " << argv[0] << " <H> <W>" << endl;
+    exit(1);
+  }
+  H = atoi(argv[1]);
+  W = atoi(argv[2]);
+  
+  init();
+  
+  time(&start);
+  spec(1, H, W, A);
+  time(&end);
+  output(H, W, A);
+  return 0;
+}
