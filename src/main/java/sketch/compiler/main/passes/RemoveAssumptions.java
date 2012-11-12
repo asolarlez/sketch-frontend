f34d5ddbd9e9829501b509d311570f75fb0615a3
Zhilei Xu <none@none>
2012-11-12 17:51:47 -0500

    new RemoveAssumptions that is more general
    --HG--
    branch : armando_prototype

--- src/main/java/sketch/compiler/main/passes/RemoveAssumptions.java
+++ src/main/java/sketch/compiler/main/passes/RemoveAssumptions.java
@@ -1,12 +1,9 @@
 package sketch.compiler.main.passes;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 import sketch.compiler.ast.core.FEReplacer;
-import sketch.compiler.ast.core.Function;
-import sketch.compiler.ast.core.Package;
 import sketch.compiler.ast.core.exprs.ExprFunCall;
 import sketch.compiler.ast.core.exprs.Expression;
 import sketch.compiler.ast.core.stmts.Statement;
@@ -23,55 +20,25 @@ public class RemoveAssumptions extends FEReplacer {
     public final static String nopName = InsertAssumptions.nopName;
 
     @Override
-    public Object visitStmtBlock(StmtBlock sb) {
-        if (sb.getStmts().size() == 1 && sb.getStmts().get(0) instanceof StmtIfThen) {
-            StmtIfThen s = (StmtIfThen) sb.getStmts().get(0);
-            if (s.getAlt() instanceof StmtBlock &&
-                    ((StmtBlock) s.getAlt()).getStmts().size() == 1)
+    public Object visitStmtIfThen(StmtIfThen s) {
+        if (s.getAlt() instanceof StmtBlock &&
+                ((StmtBlock) s.getAlt()).getStmts().size() == 1)
+        {
+            Statement t = ((StmtBlock) s.getAlt()).getStmts().get(0);
+            if (t instanceof StmtExpr &&
+                    ((StmtExpr) t).getExpression() instanceof ExprFunCall)
             {
-                Statement t = ((StmtBlock) s.getAlt()).getStmts().get(0);
-                if (t instanceof StmtExpr &&
-                        ((StmtExpr) t).getExpression() instanceof ExprFunCall)
-                {
-                    ExprFunCall c = (ExprFunCall) ((StmtExpr) t).getExpression();
-                    if (c.getName() == nopName) {
-                        return new StmtBlock(s.getCons());
+                ExprFunCall c = (ExprFunCall) ((StmtExpr) t).getExpression();
+                if (c.getName() == nopName) {
+                    Statement cons = s.getCons();
+                    if (cons != null) {
+                        return cons.accept(this);
+                    } else {
+                        return null;
                     }
                 }
             }
         }
-        return sb;
-    }
-
-    public Function changeFunction(Function f) {
-        return (Function) f.accept(this);
-    }
-
-    @Override
-    public Object visitStreamSpec(Package p) {
-        List<Function> fs = p.getFuncs();
-        // topFuncs = new HashSet<String>(fs.size());
-        // for (Function f : fs) {
-        // if (f.isSketchHarness()) {
-        // topFuncs.add(f.getName());
-        // }
-        // String g = f.getSpecification();
-        // if (g != null) {
-        // topFuncs.add(f.getName());
-        // topFuncs.add(g);
-        // }
-        // }
-        
-        List<Function> newfs = new ArrayList<Function>(fs.size());
-        for (Function f : fs) {
-            if (f.getName() != nopName) {
-                // Function newf = topFuncs.contains(f.getName()) ? changeFunction(f) : f;
-                Function newf = changeFunction(f);
-                newfs.add(newf);
-            }
-        }
-
-        return new Package(p, p.getName(), p.getStructs(), p.getVars(), newfs,
-                new ArrayList<Expression>());
+        return super.visitStmtIfThen(s);
     }
 }
--- src/runtime/include/spmd.h
+++ src/runtime/include/spmd.h
@@ -20,19 +20,37 @@ void mpiBarrier() {
   MPI_Barrier(MPI_COMM_WORLD);
 }
 
-void mpiTransfer(int size, float * src_buff, bool scond, int recipient, float * dest_buff, bool rcond) {
+/*
+int SPMD_MAX = 1;
+int SPMD_MIN = 2;
+int SPMD_SUM = 3;
+int SPMD_PROD = 4;
+*/
+static MPI_Op const OP[] = {
+  0,
+  MPI_MAX,
+  MPI_MIN,
+  MPI_SUM,
+  MPI_PROD
+};
+
+void mpiReduce(int op, int size, float * sendbuf, float recvbuf) {
+	MPI_Allreduce(sendbuf, recvbuf, size, MPI_FLOAT, OP[op], MPI_COMM_WORLD);
+}
+
+void mpiTransfer(int size, bool scond, float * sendbuf, int recipient, bool rcond, float * recvbuf) {
   static int epoch = 0;
 
   int tag = ++epoch;
   MPI_Status status;
   if (scond) {
     if (rcond) {
-      MPI_Sendrecv(src_buff, size, MPI_FLOAT, recipient, tag, dest_buff, size, MPI_FLOAT, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
+      MPI_Sendrecv(sendbuf, size, MPI_FLOAT, recipient, tag, recvbuf, size, MPI_FLOAT, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
     } else {
-      MPI_Send(src_buff, size, MPI_FLOAT, recipient, tag, MPI_COMM_WORLD);
+      MPI_Send(sendbuf, size, MPI_FLOAT, recipient, tag, MPI_COMM_WORLD);
     }
   } else {
-    MPI_Recv(dest_buff, size, MPI_FLOAT, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
+    MPI_Recv(recvbuf, size, MPI_FLOAT, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
   }
 }
 
