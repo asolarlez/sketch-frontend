df53e6af5f2d916ed017cbc5c1c64c5e5c3a0def
Zhilei Xu <none@none>
2013-04-24 09:47:11 -0400

    separate trans timers
    --HG--
    branch : armando_prototype

--- benchmarks/transpose/run/tr17.cpp
+++ benchmarks/transpose/run/tr17.cpp
@@ -1,10 +1,13 @@
 #include "tr17.h"
 namespace npb{
 void transpose_xy_z(int nx, int ny, int nz, double* a/* len = ((nx * ny) * nz) / spmdnproc */, double* b/* len = ((nx * ny) * nz) / spmdnproc */) {
+  timers[0].start();
 #ifdef __dbg__
   cout << spmdpid << ": before local" << endl;
 #endif
+  timers[1].start();
   transpose_local(nx, ny, nz, a, b);
+  timers[1].stop();
 #ifdef __dbg__
   cout << spmdpid << ": after local" << endl;
 #endif
@@ -12,41 +15,41 @@ void transpose_xy_z(int nx, int ny, int nz, double* a/* len = ((nx * ny) * nz) /
 #ifdef __dbg__
   cout << spmdpid << ": before alltoall" << endl;
 #endif
+  timers[2].start();
   spmd::spmdalltoall(sz, b, a);
+  timers[2].stop();
 #ifdef __dbg__
   cout << spmdpid << ": after alltoall" << endl;
 #endif
+  timers[3].start();
   transpose_finish(nx, ny, nz, a, b);
+  timers[3].stop();
 #ifdef __dbg__
   cout << spmdpid << ": after finish" << endl;
 #endif
+  timers[0].stop();
 }
-void transpose_local(int nx, int ny, int nz, double* a/* len = ((nx * ny) * nz) / spmdnproc */, double* b/* len = ((nx * ny) * nz) / spmdnproc */) {
+inline void transpose_local(int nx, int ny, int nz, double* a/* len = ((nx * ny) * nz) / spmdnproc */, double* b/* len = ((nx * ny) * nz) / spmdnproc */) {
   int  iend_s30 = nz / spmdnproc;
   int  jend_s32 = (nx * ny) ;
 #ifdef __dbg__
   cout << spmdpid << ": nx,ny,nz,np,iend,jend=" << nx << ' ' << ny << ' ' << nz << ' ' << spmdnproc << ' ' << iend_s30 << ' ' << jend_s32 << endl;
 #endif
-  for (int  i=0;(i) < (iend_s30);i = i + 1){
-    for (int  j=0;(j) < (jend_s32);j = j + 1){
-      int  _out_s36 = iend_s30;
-      int  _out_s38 = jend_s32;
-      b[i + (j * _out_s36)] = a[(i * _out_s38) + j];
+  for (int  i=0;(i) < (iend_s30); i++) {
+    for (int  j=0;(j) < (jend_s32); j++) {
+      b[(j * iend_s30) + i] = a[(i * jend_s32) + j];
     }
   }
 }
-void transpose_finish(int nx, int ny, int nz, double* a/* len = ((nx * ny) * nz) / spmdnproc */, double* b/* len = ((nx * ny) * nz) / spmdnproc */) {
+inline void transpose_finish(int nx, int ny, int nz, double* a/* len = ((nx * ny) * nz) / spmdnproc */, double* b/* len = ((nx * ny) * nz) / spmdnproc */) {
   int  iend_s14 = nz / spmdnproc;
   int  jend_s16 = nx * (ny / spmdnproc);
-  for (int  p=0;(p) < (spmdnproc);p = p + 1){
-    for (int  j=0;(j) < (jend_s16);j = j + 1){
-      for (int  i=0;(i) < (iend_s14);i = i + 1){
-        int  _out_s18 = iend_s14;
-        int  _out_s22 = nz;
-        int  _out_s24 = nx * (ny/spmdnproc) * (nz/spmdnproc);
-        int  _out_s28 = iend_s14;
-// compiler should be able to take p*_out_s18 out
-        b[((p * _out_s18) + i) + (j * _out_s22)] = a[((p * _out_s24) + i) + (j * _out_s28)];
+  int  _out_s24 = iend_s14 * jend_s16;
+  for (int  p=0;(p) < (spmdnproc);p = p++){
+    int ioff = p * iend_s14;
+    for (int  j=0;(j) < (jend_s16);j = j++){
+      for (int  i=0;(i) < (iend_s14);i = i++){
+        b[(j * nz) + ioff + i] = a[(p * _out_s24) + (j * iend_s14) + i];
       }
     }
   }
@@ -55,6 +58,6 @@ void transpose_finish(int nx, int ny, int nz, double* a/* len = ((nx * ny) * nz)
 }
 namespace spmd{
 
-void spmdalltoall(int sz, double* src/* len = sz * spmdnproc */, double* dst/* len = sz * spmdnproc */) { mpiAlltoall(sz, src, dst); }
+inline void spmdalltoall(int sz, double* src/* len = sz * spmdnproc */, double* dst/* len = sz * spmdnproc */) { mpiAlltoall(sz, src, dst); }
 
 }
